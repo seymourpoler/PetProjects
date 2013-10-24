29d6545901ca9415805d627675d508b0eb590388
Seymour Poler <seymourpoler@gmail.com>
2013-10-24 23:09:03 +0200

    adding some functionality to DELETE sentences

--- SQUEL/dotNET/Squel/Squel.Test/Squel.Delete.Test.cs
+++ SQUEL/dotNET/Squel/Squel.Test/Squel.Delete.Test.cs
@@ -15,5 +15,18 @@ namespace Squel.Test
 
             Assert.AreEqual(expected, sql.ToString());
         }
+
+        [TestMethod]
+        public void Should_Return_Simple_Delete_From_Table_With_Some_Where()
+        {
+            var expected = "DELETE FROM students WHERE (id > 5) AND (id < 102)";
+            var sql = new SQL();
+            sql.Delete()
+                .From("students")
+                .Where("id > 5")
+                .Where("id < 102");
+
+            Assert.AreEqual(expected, sql.ToString());
+        }
     }
 }
--- SQUEL/dotNET/Squel/Squel/DeleteQuery.cs
+++ SQUEL/dotNET/Squel/Squel/DeleteQuery.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Squel
 {
@@ -6,18 +7,40 @@ namespace Squel
     {
         private const string DELETE = "DELETE ";
         private const string FROM = "FROM ";
+        private const string WHERE = " WHERE ";
 
         private string _from;
+        private IList<string> _where;
+
+        public DeleteQuery()
+        {
+            _from = string.Empty;
+            _where = new List<string>();
+        }
 
         public string ToSQLString()
         {
-            return string.Format("{0}{1}{2}", DELETE, FROM, _from);
+            var where = GetStringWhere();
+            return string.Format("{0}{1}{2}{3}", DELETE, FROM, _from, where);
         }
 
         public DeleteQuery From(string from)
         {
             _from = from;
             return this;
+        }
+
+        public DeleteQuery Where(string where)
+        {
+            var expression = string.Format("({0})", where);
+            _where.Add(expression);
+            return this;
+        }
+
+        private string GetStringWhere()
+        {
+            var where = Map(_where, " AND ");
+            return GetString(WHERE, where);
         }
     }
 }
