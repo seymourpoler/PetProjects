21e4d2100edab3530e4eaf72a459d2dd15fb24db
Luis_Rodriguez_Alejos <luis.rodriguez@adesis.com>
2014-10-17 13:34:54 +0200

    tests controls exceptions

--- TODO/OwinWebApiRedis/TodoList/TodoList.Tests/UI/Controllers/TodoControllerTests.cs
+++ TODO/OwinWebApiRedis/TodoList/TodoList.Tests/UI/Controllers/TodoControllerTests.cs
@@ -4,6 +4,9 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TodoList.Console.UI.Controllers;
 using TodoList.Console.Domain.Services;
 using System.Net;
+using System.Web.Http;
+using System.Net.Http;
+using System.Web.Http.Hosting;
 
 namespace TodoList.Tests.UI.Controllers
 {
@@ -12,10 +15,20 @@ namespace TodoList.Tests.UI.Controllers
     {
         TodoController _todoController;
         Mock<ITasksService> _tasksService;
+
         [TestInitialize]
         public void setUp()
         {
+            _tasksService = new Mock<ITasksService>();
             _todoController = new TodoController(_tasksService.Object);
+            LoadRequestInController(_todoController);
+        }
+
+        protected void LoadRequestInController(ApiController controller)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, string.Empty);
+            controller.Request = request;
+            controller.Request.Properties[HttpPropertyKeys.HttpConfigurationKey] = new HttpConfiguration(); ;
         }
 
         [TestMethod]
@@ -24,6 +37,16 @@ namespace TodoList.Tests.UI.Controllers
             _tasksService.Setup(x => x.GetAll()).Throws(new Exception());
 
             var response = _todoController.Get();
+
+            Assert.AreEqual(HttpStatusCode.InternalServerError, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void TodoController_Get()
+        {
+            _tasksService.Setup(x => x.GetById(It.IsAny<Guid>())).Throws(new Exception());
+
+            var response = _todoController.Get(Guid.NewGuid());
 
             Assert.AreEqual(HttpStatusCode.InternalServerError, response.StatusCode);
         }
