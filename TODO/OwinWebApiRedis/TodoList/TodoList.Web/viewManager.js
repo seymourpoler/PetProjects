f63b358bab781c0ec9b9efe84c60170f0247e64e
Seymour Poler <seymourpoler@gmail.com>
2014-10-22 21:51:09 +0200

    full Update feature

--- TODO/OwinWebApiRedis/TodoList/TodoList.Web/viewManager.js
+++ TODO/OwinWebApiRedis/TodoList/TodoList.Web/viewManager.js
@@ -5,27 +5,48 @@ function ViewManager(taskService)
 	function load(){
 		$.when(_taskService.getAll())
 		.done(function(tasks){
-		$('#todo_form')[0].reset();
-			_.each(tasks, function(task){
-				$('#todo_list').prepend('<div>' + task.title + ' <a id=' + task.id + ' href=\'#\' class="remove_task">Remove</a> <a id=' + task.id + ' href=\'#\' class="update_task">Update</a></div>');
-				$('.remove_task').click( function() {
+			cleanAll();
+			loadAllTasksIntoControls(tasks);
+		});
+	}
+
+	function setUpRemoveTask(){
+		$('.remove_task').click( function() {
 					var idTask = $(this).attr('id');
 					_taskService.remove(idTask);
 					$(this).parent().fadeOut();
+					cleanTaskControls();
 				});
-				$('.update_task').click( function() {
+	}
+
+	function setUpUpdateTask(){
+		$('.update_task').click( function() {
 					var idTask = $(this).attr('id');
 					loadTaskIntoControls(idTask);
 				});
-				$('#btnUpdate').click( function() {
+	}
+
+	function setUpButtonUpdate(){
+		$('#btnUpdate').click( function() {
 					var idTask = $(this).attr('id');
 					var task = getTaskFromControls();
-					_taskService.update(task);
-					cleanControls();
+					$.when(_taskService.update(task))
+					.done(function(){
+						cleanTaskControls();
+						load();
+					})
 				});
-			});
-		});
 	}
+
+	function loadAllTasksIntoControls(tasks){
+		_.each(tasks, function(task){
+				$('#todo_list').prepend('<div>' + task.title + ' ' + task.description + ' <a id=' + task.id + ' href=\'#\' class="remove_task">Remove</a> <a id=' + task.id + ' href=\'#\' class="update_task">Update</a></div>');
+				setUpRemoveTask();
+				setUpUpdateTask();
+				setUpButtonUpdate();
+			});
+	}
+
 
 	function loadTaskIntoControls(idTask)
 	{
@@ -46,7 +67,12 @@ function ViewManager(taskService)
 		return task;
 	}
 
-	function cleanControls(){
+	function cleanAll(){
+		$('#todo_list').empty();
+		cleanTaskControls();
+	}
+	function cleanTaskControls(){
+		$('#txtId').val('');
 		$('#txtTitle').val('');
 		$('#txtDescription').val('');	
 	}
