d57df9a41ce555b5513b93f9c2e8899f2937db40
Luis_Rodriguez_Alejos <luis.rodriguez@adesis.com>
2014-10-29 17:03:07 +0100

    validations inside services

--- TODO/OwinWebApiRedis/TodoList/TodoList.Console/UI/Models/TaskModel.cs
+++ TODO/OwinWebApiRedis/TodoList/TodoList.Console/UI/Models/TaskModel.cs
@@ -6,11 +6,9 @@ namespace TodoList.Console.UI.Models
     public class TaskModel
     {
         public Guid id { get; set; }
-        [Required]
         public string title { get; set; }
         public string description { get; set; }
 
-        [Range(1,3)]
         public StateTaskModel state { get; set; }
 
         public TaskModel()
