41d9429c7212c93584b3f2888760504bdd1abc31
Seymour Poler <seymourpoler@gmail.com>
2015-05-06 21:35:04 +0200

    adding take functionality

--- Linqjs/source/sut.js
+++ Linqjs/source/sut.js
@@ -1,7 +1,21 @@
-function removeElementInArray(number, vector){
-	return vector.filter(function(element){
-		return (element != number);
-	});
+Array.prototype.count = function() {
+  return this.length;
+};
+
+Array.prototype.sum = function(){
+  var result = 0;
+  for (var count = 0; count < this.length; count++) {
+    result = result + this[count];
+  };
+  return result;
 }
 
-exports.remove = removeElementInArray;
+Array.prototype.take = function(position) {
+	if(position > this.length){
+		return [];
+	}
+
+	return this.slice(0, position);
+};
+
+exports.remove = function(){};
--- Linqjs/test/test.js
+++ Linqjs/test/test.js
@@ -1,18 +1,36 @@
 var sut = require("../source/sut.js"),
  expect = require("expect.js");
-Array.prototype.Count = function(first_argument) {
-  return this.length;
-};
 
+describe("linq in javascript using TDD", function(){
+  describe(".count()", function(){
+    it("Given empty array when call Count method then returns zero ", function(){
+      var numbers =[];
+      expect(numbers.count()).to.eql(0);
+    });
 
-describe("linq in jabascript using TDD", function(){
-  it("Given empty array when call Count method then returns zero ", function(){
-    var numbers =[];
-    expect(numbers.Count()).to.eql(0);
+    it("Given array when call Count method then returns the number of elements ", function(){
+      var numbers =[1,2,3,4];
+      expect(numbers.count()).to.eql(4);
+    });
   });
-
-  it("Given array when call Count method then returns the number of elements ", function(){
-    var numbers =[1,2,3,4];
-    expect(numbers.Count()).to.eql(4);
+  describe(".sum()", function(){
+    it("Given array when call Sum method then returns Zero ", function(){
+      var numbers =[];
+      expect(numbers.sum()).to.eql(0);
+    });
+    it("Given array  with numbers when call Sum method then returns the sumatory", function(){
+      var numbers =[1,2,3,4];
+      expect(numbers.sum()).to.eql(10);
+    });
+  });
+  describe(".take(<number>)", function(){
+    it("Given array when call Sum method then returns Zero ", function(){
+      var numbers =[];
+      expect(numbers.take(3)).to.eql([]);
+    });
+    it("Given array  with numbers when call Sum method then returns the sumatory", function(){
+      var numbers =[5,3,7,9];
+      expect(numbers.take(2)).to.eql([5,3]);
+    });
   });
 });
