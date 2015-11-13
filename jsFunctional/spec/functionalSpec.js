5a748984d51f245251cab26a779b2dd109db6cb7
Seymour Poler <seymourpoler@gmail.com>
2015-11-13 22:09:06 +0100

    some functional functions in javascript as: map, reduce, filter and forEach

---
+++ jsFunctional/spec/functionalSpec.js
@@ -0,0 +1,82 @@
+describe("functional programming", function(){
+	describe("map function", function(){
+		it("return array with mapped data", function(){
+			var data = [1,2,3,4,5];
+			var resultExpected = [2,3,4,5,6];
+			
+			var mappedResult = map(data, function(number){
+				return number + 1;
+			});
+
+			expect(resultExpected).to.eql(mappedResult);
+		});
+	});
+	describe("reduce function", function(){
+		it("return the sum of numbers", function(){
+			var data = [1,2,3,4,5];
+			var resultExpected = 15;
+			var initialValue = 0;
+			
+			var reducedResult = reduce(data, initialValue, function(acumulativeNumber, eachNumber){
+				return acumulativeNumber + eachNumber;
+			});
+
+			expect(resultExpected).to.eql(reducedResult);
+		});
+	});
+	describe("filter function", function(){
+		it("return an array only with numbers", function(){
+			var data = [1,'2',3,'4',5];
+			var resultExpected = [1, 3, 5];
+	
+			var filteredResult = filter(data, function(eachElement){
+				if(typeof eachElement === "number"){
+					return true;
+				}
+				return false;
+			});
+
+			expect(resultExpected).to.eql(filteredResult);
+		});
+	});
+	describe("combine filter, map & reduce", function(){
+		it("return the result of combination of filter, mal and reduce", function(){
+			var data = [1,2,3,4,5,6];
+			var resultExpected = 15;
+
+			var result = reduce(
+								map(
+									filter(
+										data, functionForFilter
+									), 
+									functionForMap
+								), 
+								0, functionForReduce
+							);
+
+			expect(resultExpected).to.eql(result);
+		});
+		function functionForMap(y){
+			return y + 1;
+		}
+		function functionForFilter(x){
+			return	x % 2 == 0;
+		}
+		function functionForReduce(acumulative, z){ 
+			return acumulative + z;
+		}
+	});
+	describe("forEach function", function(){
+		it("return the concatenation of the array", function(){
+			var data = ['he', 'llo ', 'ever','ybody']
+			var expectedResult = 'hello everybody';
+			var concatenatedResult = '';
+
+			forEach(data, function(text){
+				concatenatedResult += text;
+			});
+
+			expect(expectedResult).to.eql(concatenatedResult);
+		});
+	});
+});
---
+++ jsFunctional/src/functional.js
@@ -0,0 +1,31 @@
+function map(arrayOfData, handler){
+	var mappedResult = [];
+	for (var position = 0; position < arrayOfData.length; position++) {
+		mappedResult.push(handler(arrayOfData[position]));
+	};
+	return mappedResult;
+}
+
+function reduce(arrayOfData, initialValue, handler){
+	var reducedResult = initialValue;
+	for (var position = 0; position < arrayOfData.length; position++) {
+		reducedResult = handler(reducedResult, arrayOfData[position]);
+	}
+	return reducedResult;
+}
+
+function filter(arrayOfData, handler){
+	var filteredResult = [];
+	for (var position = 0; position < arrayOfData.length; position++) {
+		if(handler(arrayOfData[position])){
+			filteredResult.push(arrayOfData[position]);
+		}
+	};
+	return filteredResult;
+}
+
+function forEach(arrayOfData, handler){
+	for (var position = 0; position < arrayOfData.length; position++) {
+		handler(arrayOfData[position])
+	};
+}
