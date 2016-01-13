f80cd390ccbb63289450338b9083ed3af6acc977
Seymour Poler <seymourpoler@gmail.com>
2016-01-13 20:24:58 +0100

    adding find, findOne, and remove function

--- example-mongodb/program.js
+++ example-mongodb/program.js
@@ -3,8 +3,25 @@ var TheRepo = require('./userRepository');
 var userRepository = new TheRepo(configuration);
 
 console.log(userRepository);
+var user = {
+  firstName:'John',
+  lastName:'Keeve',
+  age:12,
+  profession: 'software developer'
+};
+userRepository.save(user);
 
 userRepository.find(findHandler);
-function findHandler(data){
-  console.log('data: ', data);
+function findHandler(items){
+  console.log('all items: ', items);
+}
+userRepository.findOne(user, findOneHandler);
+function findOneHandler(item){
+  console.log('One item: ', item);
+}
+
+userRepository.delete();
+userRepository.find(findHandler);
+function findHandler(items){
+  console.log('all items again: ', items);
 }
--- example-mongodb/userRepository.js
+++ example-mongodb/userRepository.js
@@ -5,15 +5,45 @@ function UserRepository(configuration){
   const collectionName = 'user';
 
   this.save = function(user){
+    mongoClient.connect(configuration.getConnectionString(), function(err, db) {
+      if(err) { return console.log(err); }
+
+      var collection = db.collection(collectionName);
+      collection.insert(user, function(err, result) {
+        if(err) { return console.log('insert:',err); }
+        console.log('insert: ', result);
+      });
+      db.close();
+    });
+  };
+  this.delete = function(){
+    mongoClient.connect(configuration.getConnectionString(), function(err, db) {
+      if(err) { return console.log(err); }
+
+      var collection = db.collection(collectionName);
+      collection.remove();
+      db.close();
+    });
+  };
+
+  this.deleteOne = function(user){
     throw 'not implemented';
   };
-  this.delete = function(user){
-    throw 'not implemented';
-  };
+
   this.findOne = function(user, findOneHandler){
 
+    mongoClient.connect(configuration.getConnectionString(), function(err, db) {
+      if(err) { return console.log(err); }
+
+      var collection = db.collection(collectionName);
+      collection.findOne(user, function(err, item) {
+        if(err) { return console.log('findOne',err); }
+        findOneHandler(item);
+        db.close();
+      });
+    });
   };
-  
+
   this.find = function(findHandler){
     mongoClient.connect(configuration.getConnectionString(), function(err, db) {
       if(err) { return console.log(err); }
