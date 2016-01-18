3939facedb61d65072a6da2e22ef8fda9abdf32f
Seymour Poler <seymourpoler@gmail.com>
2016-01-18 23:46:27 +0100

    adding methods to taskService

---
+++ example-express-mongodb/Task/Services/taskService.js
@@ -0,0 +1,12 @@
+'use strict';
+var taskRepository = require('../Repositories/taskRepository');
+
+function TaskService(){
+  this.save = funcion(task){
+    throw 'not implemented';
+  };
+
+  this.delete = function(){
+    throw 'not implemented';
+  };
+}
