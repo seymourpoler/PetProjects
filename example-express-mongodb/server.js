9e693a8338a8848d77d46d19242cfd518be1d3b5
Seymour Poler <seymourpoler@gmail.com>
2016-03-31 20:50:13 +0200

    remove duplicated code

--- example-express-mongodb/server.js
+++ example-express-mongodb/server.js
@@ -4,7 +4,6 @@ var app = express();
 var bodyParser = require('body-parser');
 var path = require("path");
 var configuration = require('./configuration');
-//var taskApiRouter = require('./Task/Router/apiRouter');
 var userRouterConfigurator = require('./User/Router/routerConfigurator');
 var taskRouterConfigurator = require('./Task/Router/routerConfigurator');
 
@@ -13,7 +12,6 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || configuration.getServerPort();
 
-//app.use('/api', taskApiRouter);
 userRouterConfigurator.configure(app);
 taskRouterConfigurator.configure(app);
 
@@ -24,12 +22,7 @@ app.use('/Home/Scripts', express.static(__dirname + '/Home/Scripts'));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/Home/Views/index.html'));
 });
-/*
-app.use('/Task/Scripts', express.static(__dirname + '/Task/Scripts'));
-app.get('/tasks', function(req, res) {
-    res.sendFile(path.join(__dirname + '/Task/Views/index.html'));
-});
-*/
+
 app.get('/about',function(req,res){
   res.sendFile(path.join(__dirname + '/About//Views/index.html'));
 });
