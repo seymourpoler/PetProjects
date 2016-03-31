19d64cc6e22bf7d9689500ae408279c1a9cb0a1e
Seymour Poler <seymourpoler@gmail.com>
2016-03-31 20:48:14 +0200

    remove duplicated code

--- example-express-mongodb/User/Router/apiRouter.js
+++ example-express-mongodb/User/Router/apiRouter.js
@@ -3,8 +3,6 @@ var express = require('express');
 var router = express.Router();
 var findController = require('../Controllers/findController');
 
-var router = express.Router();
-
 router.get('/', function(req, res) {
     res.json({ message: 'Wellcome to our api!' });
 });
