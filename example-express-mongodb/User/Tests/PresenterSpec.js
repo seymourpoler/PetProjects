b13a821d0c328610523ab8a7f0765be8aa568174
Seymour Poler <seymourpoler@gmail.com>
2016-04-04 19:48:12 +0200

    when loads users, shows users

--- example-express-mongodb/User/Tests/PresenterSpec.js
+++ example-express-mongodb/User/Tests/PresenterSpec.js
@@ -1,16 +1,20 @@
 describe("View Presenter", function(){
   var client, view, presenter;
+
   beforeEach(function(){
     client = {
       getUsers: function(){}
     };
     view = {
+      subscribesToUserAddingEvent: function(){},
       showErrorMessage:function(){},
-      subscribesToUserAddingEvent: function(){}
+      loadUsers: function(){}
     };
   });
+
   describe("when loads users", function(){
-    it("show error message if there is an error", function(){
+
+    it("shows error message if there is an error", function(){
       spyOn(client, "getUsers").and.callFake(function(successhandler, errorHandler){
         errorHandler();
       });
@@ -19,6 +23,17 @@ describe("View Presenter", function(){
       presenter = new UserPresenter(view, client);
 
       expect(view.showErrorMessage).toHaveBeenCalled();
+    });
+
+    it("shows users", function(){
+      spyOn(client, "getUsers").and.callFake(function(successhandler, errorHandler){
+        successhandler();
+      });
+      spyOn(view, "loadUsers");
+
+      presenter = new UserPresenter(view, client);
+
+      expect(view.loadUsers).toHaveBeenCalled();
     });
   });
 });
