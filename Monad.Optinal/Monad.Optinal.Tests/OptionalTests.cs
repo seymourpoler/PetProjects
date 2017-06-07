d64d575a2ce8a9e2e76fc3d3821b4f62fac9e109
Seymour Poler <seymourpoler@gmail.com>
2017-06-07 06:17:32 +0200

    green ReturnsSomeWhenThereIsSomeValue

---
+++ Monad.Optinal/Monad.Optinal.Tests/OptionalTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace Monad.Optinal.Tests
+{
+	[TestFixture]
+	public class OptionalTests
+	{
+		[Test]
+		public void ReturnsSomeWhenThereIsSomeValue()
+		{
+			var some = Optional.From ("Tome");
+
+			some.Should ().BeOfType<Some<string>> ();
+		}
+	}
+}
+
---
+++ Monad.Optinal/Monad.Optinal/Optional.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Monad.Optinal
+{
+	public interface IOptional<T> where T : class
+	{
+		
+	}
+
+	public class Optional
+	{
+		public static IOptional<T> From<T>(T value) where T : class
+		{
+			if(value == default(T)){
+				return new None<T> ();
+			}
+			return new Some<T> (value);
+		}
+	}
+
+	public class Some<T> : IOptional<T> where T : class{
+		private readonly T _value;
+
+		public Some(T value){
+			_value = value;	
+		}
+	}
+
+	public class None<T> : IOptional<T> where T : class{}
+}
+
