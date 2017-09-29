0caa8105278e0a3cd278bb9a7a81876660b10da0
root <root@DesarrolloLuis.citasonnet.local>
2017-09-29 16:21:51 +0200

    green PublishsMessageWithOneHandler

--- SimplePubSub/SimplePubSub/Hub.cs
+++ SimplePubSub/SimplePubSub/Hub.cs
@@ -1,17 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SimplePubSub
 {
     public class Hub
     {
-        public void Subscribe<T>(Action<T> action) where T : class
+        private Dictionary<string, Delegate> handlers;
+        private object locker;
+        public Hub()
         {
-            throw new NotImplementedException();
+            locker = new object();
+            handlers = new Dictionary<string, Delegate>();
         }
 
-        public  void Subscribe<T>(T message) where T : class
+        public void Subscribe<T>(Action<T> action) where T : class
         {
-            throw new NotImplementedException();
+            lock (locker)
+            {
+                handlers.Add(typeof(T).ToString(), action);
+            }
+        }
+
+        public  void Publish<T>(T message) where T : class
+        {
+            lock(locker)
+            {
+                var handler = handlers[typeof(T).ToString()];
+                if (handler == null) return;
+                ((Action<T>)handler)(message);
+            }
         }
 
         public void Unsubscribe<T>(Action<T> handler = null)
