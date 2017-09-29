4f09cee93eb4ea0eeb532bb29257e64fdeeb2533
root <root@DesarrolloLuis.citasonnet.local>
2017-09-29 17:27:02 +0200

    adding Shouldly

--- SimplePubSub/SimplePubSubTests/HubTests.cs
+++ SimplePubSub/SimplePubSubTests/HubTests.cs
@@ -1,5 +1,7 @@
 ﻿using NUnit.Framework;
 using SimplePubSub;
+using System;
+using Shouldly;
 
 namespace SimplePubSubTests
 {
@@ -11,6 +13,34 @@ namespace SimplePubSubTests
         public void SetUp()
         {
             hub = new Hub();
+        }
+
+        [Test]
+        public void PublishsMessageWithOneHandler()
+        {
+            var result = String.Empty;
+            var contentMessage = "Message";
+            hub.Subscribe<Message>(x => result = x.Content);
+
+            hub.Publish(new Message { Content = contentMessage });
+
+            result.ShouldBe(contentMessage);
+        }
+
+        [Test]
+        public void DoeSNothingWhenThereIsNoHandler()
+        {
+            var result = String.Empty;
+            var contentMessage = "Message";
+
+            hub.Publish(new Message { Content = contentMessage });
+
+            result.ShouldNotBe(contentMessage);
+        }
+
+        internal class Message
+        {
+            public string Content { get; set; }
         }
     }
 }
