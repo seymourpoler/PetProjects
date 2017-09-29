7cdd787d228eaec1d8b0702bbede14c50e0c29be
root <root@DesarrolloLuis.citasonnet.local>
2017-09-29 14:41:15 +0200

    nunit

---
+++ SimplePubSub/SimplePubSub/SimplePubSub/Hub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SimplePubSub
+{
+    public class Hub
+    {
+        public void Subscribe<T>(Action<T> action) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public  void Subscribe<T>(T message) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unsubscribe<T>(Action<T> handler = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SimplePubSub/SimplePubSub/SimplePubSubTests/HubTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using SimplePubSub;
+
+namespace SimplePubSubTests
+{
+    [TestFixture]
+    public class HubTests
+    {
+        private Hub hub;
+        [SetUp]
+        public void SetUp()
+        {
+            hub = new Hub();
+        }
+    }
+}
