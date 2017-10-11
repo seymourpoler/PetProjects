2c6f6e3f455287bf024b3f66828af13c6fae6ce5
Seymour Poler <seymourpoler@gmail.com>
2017-10-11 19:39:10 +0200

    ignore: fotmat

--- SimplePubSub/SimplePubSub/Hub.cs
+++ SimplePubSub/SimplePubSub/Hub.cs
@@ -7,6 +7,7 @@ namespace SimplePubSub
     {
         private Dictionary<string, Delegate> handlers;
         private object locker;
+
         public Hub()
         {
             locker = new object();
