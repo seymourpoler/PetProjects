b66fec93a58158c9289f5e1154cb92db25416926
Seymour Poler <seymourpoler@gmail.com>
2017-10-11 19:45:08 +0200

    adding HttpRequestBuilder project

---
+++ HttpRequestBuilder/HttpRequestBuilder.Tests/BuilderTest.cs
@@ -0,0 +1,117 @@
+﻿using NUnit.Framework;
+using HttpRequestBuilder;
+using FluentAssertions;
+using System.Web;
+using System.Web.Mvc;
+using Moq;
+
+namespace HttpRequestBuilder.Tests
+{
+	[TestFixture]
+	public class BuilderTest
+	{
+		[Test]
+		public void BuildHttpRequestBase()
+		{
+			var result = new Builder().Build ();
+
+			result.Should ().BeOfType<Mock<HttpRequestBase>> ();
+		}
+
+		[Test]
+		public void BuildHttpRequestBaseWithHeader()
+		{
+			var header = "header";
+			var headerValue = "headerValue";
+
+			var result = new Builder ()
+				.WithHeader (header, headerValue)
+				.Build ();
+
+			result.Object.Headers [header].Should ().Be (headerValue);
+		}
+			
+		[Test]
+		public void BuildHttpRequestBaseWithMoreThanOneHeader()
+		{
+			var headerOne = "headerOne";
+			var headerValueOne = "headerValueOne";
+			var headerTwo = "headerTwo";
+			var headerValueTwo = "headerValueTwo";
+
+			var result = new Builder ()
+				.WithHeader (headerOne, headerValueOne)
+				.WithHeader (headerTwo, headerValueTwo)
+				.Build ();
+
+			result.Object.Headers [headerOne].Should ().Be (headerValueOne);
+			result.Object.Headers [headerTwo].Should ().Be (headerValueTwo);
+		}
+
+		[Test]
+		public void BuildHttpRequestBaseWithContentType()
+		{
+			var contentType = "ContentType";
+
+			var result = new Builder ()
+				.WithContentType (contentType)
+				.Build ();
+
+			result.Object.ContentType.Should ().Be (contentType);
+		}
+
+		[Test]
+		public void BuildHttpRequestBaseWithHttpMethod()
+		{
+			var httpMethod = "httpMethod";
+
+			var result = new Builder ()
+				.WithHttpMethod (httpMethod)
+				.Build ();
+
+			result.Object.HttpMethod.Should ().Be (httpMethod);
+		}
+
+		[Test]
+		public void AddToControllerHttpRequestWithContentType()
+		{
+			var contentType = "Content-Type";
+			var controller = new ProductsController ();
+
+			var controllerResult = new Builder ()
+				.WithContentType (contentType)
+				.AddTo (controller);
+
+			controllerResult.Request.ContentType.Should ().Be (contentType);
+		}
+
+		[Test]
+		public void AddToControllerHttpRequestWithHttpMethod()
+		{
+			var httpMethod = "httpMethod";
+			var controller = new ProductsController ();
+
+			var controllerResult = new Builder ()
+				.WithHttpMethod (httpMethod)
+				.AddTo (controller);
+
+			controllerResult.Request.HttpMethod.Should ().Be (httpMethod);
+		}
+
+		[Test]
+		public void AddToControllerHttpRequestWithHeader()
+		{
+			var header = "header";
+			var headerValue = "headerValue";
+			var controller = new ProductsController ();
+
+			var controllerResult = new Builder ()
+				.WithHeader (header, headerValue)
+				.AddTo (controller);
+
+			controllerResult.Request.Headers [header].Should ().Be (headerValue);
+		}
+	}
+
+	class ProductsController : Controller{}
+}
---
+++ HttpRequestBuilder/HttpRequestBuilder/Builder.cs
@@ -0,0 +1,75 @@
+﻿using Moq;
+using System.Web;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Web.Routing;
+
+namespace HttpRequestBuilder
+{
+	public class Builder
+	{
+		private Dictionary<string, string> _headerValues;
+		private string _contentType;
+		private string _httpMethod;
+
+		public Builder(){
+			_headerValues = new Dictionary<string, string>();
+			_contentType = "text/html";
+			_httpMethod = "GET";
+		}
+
+		public Mock<HttpRequestBase> Build()
+		{
+			var request =  new Mock<HttpRequestBase> ();
+			if(_headerValues.Count > 0){
+			request
+				.Setup (x => x.Headers)
+				.Returns (BuildHeaders ());
+			}
+			request
+				.Setup (x => x.ContentType)
+				.Returns (_contentType);
+			request
+				.Setup (x => x.HttpMethod)
+				.Returns (_httpMethod);
+			return request;
+		}
+
+		public Controller AddTo(Controller controller)
+		{
+			var requestBase = Build ();
+			var context = new Mock<HttpContextBase>();
+			context.Setup(x => x.Request).Returns(requestBase.Object);
+			controller.ControllerContext = new ControllerContext(context.Object, new RouteData(), controller);
+			return controller;
+		}
+
+		private NameValueCollection BuildHeaders()
+		{
+			var result = new NameValueCollection ();
+			foreach(var header in _headerValues){
+				result.Add (header.Key, header.Value);
+			}
+			return result;
+		}
+
+		public Builder WithHeader(string header, string value){
+			_headerValues.Add (header, value);
+			return this;
+		}
+
+		public Builder WithContentType(string contentType)
+		{
+			_contentType = contentType;
+			return this;
+		}
+
+		public Builder WithHttpMethod(string httpMethod)
+		{
+			_httpMethod = httpMethod;
+			return this;
+		}
+	}
+}
+
