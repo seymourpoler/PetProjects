fda9ad108ddcb8de9fc73f9520c19b73089edb31
Seymour Poler <seymourpoler@gmail.com>
2018-01-19 07:44:18 -0500

    green ReturnTrueWhenIsNull

---
+++ ExtensionMethods/ExtensionMethods/ReadOnlyCollectionExtensions.IsEmpty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExtensionMethods
+{
+    public static class ReadOnlyCollectionExtensions
+    {
+        public static bool IsEmpty<T>(this IReadOnlyCollection<T> collection)
+        {
+            if (collection == null)
+            {
+                return true;
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethodsTests/ReadOnlyCollectionExtensionsTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using ExtensionMethods;
+using Shouldly;
+using Xunit;
+
+namespace ExtensionMethodsTests
+{
+    public class ReadOnlyCollectionExtensionsTests
+    {
+        [Fact]
+        public void ReturnTrueWhenIsNull()
+        {
+            IReadOnlyCollection<string> values = null;
+
+            var result = values.IsEmpty();
+            
+            result.ShouldBeTrue();
+        }
+    }
+}
