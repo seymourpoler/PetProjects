6916c92b5c328b31ffb522ae0361cf7d2a9ad5be
Luis Rodriguez <lrodriguez@gowaiicorp.com>
2018-02-07 20:31:33 +0100

    green ReturnsFormattedText

---
+++ Gambon/Gambon/test/unit/GambonUnitTest/Core/StringExtensionsTests.cs
@@ -0,0 +1,16 @@
+﻿using Gambon.Core;
+using Xunit;
+
+namespace Gambon.Test.Unit.Core
+{
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void ReturnsFormattedText()
+        {
+            var result = "test {0}".FormatWith("one");
+
+            Assert.Equal("test one", result);
+        }
+    }
+}
