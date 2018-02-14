3ae8f9a9487da77a188712b6f7296a8c007a49e1
Seymour Poler <seymourpoler@gmail.com>
2018-02-14 10:59:35 +0100

    format

--- Gambon/Gambon/src/Gambon/SqlServer/SqlExecutor.cs
+++ Gambon/Gambon/src/Gambon/SqlServer/SqlExecutor.cs
@@ -1,48 +1,54 @@
-﻿using System;
+﻿using Gambon.Core;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-using Gambon.Core;
 using System.Linq;
 
 namespace Gambon.SqlServer
 {
-	public class SqlExecutor
-	{
-		private readonly SqlConnectionFactory sqlConnectionFactory;
-		
-		public SqlExecutor(SqlConnectionFactory sqlConnectionFactory)
-		{
-			this.sqlConnectionFactory = sqlConnectionFactory;
-		}
-		
-		public IEnumerable<dynamic> ExecuteReader(string sql){
-			using(var connection = sqlConnectionFactory.Create()){
-				var command = new SqlCommand(sql, connection);
-				var dataReader = command.ExecuteReader();
-				 while (dataReader.Read())
-				 {
-					yield return dataReader.ToDynamic();
-				 }
-			}
-		}
-		
-		public dynamic ExecuteFirstOrDefault(string sql){
-			return ExecuteReader(sql).FirstOrDefault();
-		}
-		
-		public int ExecuteNonQuery(string sql){
-			using(var connection = sqlConnectionFactory.Create()){
-				var command = new SqlCommand(sql, connection);
-				return command.ExecuteNonQuery();
-			}
-		}
+    public class SqlExecutor
+    {
+        private readonly SqlConnectionFactory sqlConnectionFactory;
 
-		public dynamic ExecuteScalar(string sql){
-			using(var connection = sqlConnectionFactory.Create()){
-				return new SqlCommand(sql, connection)
-					.ExecuteScalar()
-					.ToDynamic();
-			}
-		}
-	}
+        public SqlExecutor(SqlConnectionFactory sqlConnectionFactory)
+        {
+            this.sqlConnectionFactory = sqlConnectionFactory;
+        }
+
+        public IEnumerable<dynamic> ExecuteReader(string sql)
+        {
+            using (var connection = sqlConnectionFactory.Create())
+            {
+                var command = new SqlCommand(sql, connection);
+                var dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    yield return dataReader.ToDynamic();
+                }
+            }
+        }
+
+        public dynamic ExecuteFirstOrDefault(string sql)
+        {
+            return ExecuteReader(sql).FirstOrDefault();
+        }
+
+        public int ExecuteNonQuery(string sql)
+        {
+            using (var connection = sqlConnectionFactory.Create())
+            {
+                var command = new SqlCommand(sql, connection);
+                return command.ExecuteNonQuery();
+            }
+        }
+
+        public dynamic ExecuteScalar(string sql)
+        {
+            using (var connection = sqlConnectionFactory.Create())
+            {
+                return new SqlCommand(sql, connection)
+                    .ExecuteScalar()
+                    .ToDynamic();
+            }
+        }
+    }
 }
