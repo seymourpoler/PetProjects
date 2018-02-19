d6ddc3e5eeaf8ca27ebb1161358b930343ab397a
Seymour Poler <seymourpoler@gmail.com>
2018-02-19 14:32:38 +0100

    adding files

---
+++ Gambon/Gambon/src/Gambon/SqlServer/SqlExecutorWithGeneric.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Gambon.SqlServer
+{
+    public class SqlExecutorWithGeneric
+    {
+        private readonly SqlConnectionFactory sqlConnectionFactory;
+
+        public SqlExecutorWithGeneric(SqlConnectionFactory sqlConnectionFactory)
+        {
+            this.sqlConnectionFactory = sqlConnectionFactory;
+        }
+
+        public IEnumerable<T> ExecuteReader<T>(string sql) where T : class
+        {
+            using (var connection = sqlConnectionFactory.Create())
+            {
+                var command = new SqlCommand(cmdText: sql, connection: connection);
+                var dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    yield return dataReader.To<T>();
+                }
+            }
+        }
+    }
+}
---
+++ Gambon/Gambon/test/integration/GambonIntegrationTest/SqlServer/SqlExecutorWithGenericTests.cs
@@ -0,0 +1,37 @@
+﻿using Gambon.SqlServer;
+using System.Collections.Generic;
+using Xunit;
+
+
+namespace Gambon.Test.Integration.SqlServer
+{
+    public class SqlExecutorWithGenericTests
+    {
+        private AppConfiguration configuration;
+        private SqlConnectionFactory sqlConnectionFactory;
+        private SqlExecutorWithGeneric sqlExecutor;
+
+        public SqlExecutorWithGenericTests()
+        {
+            configuration = new AppConfiguration();
+            sqlConnectionFactory = new SqlConnectionFactory(configuration);
+            sqlExecutor = new SqlExecutorWithGeneric(sqlConnectionFactory);
+        }
+
+        [Fact]
+        public void ReturnsAllFields()
+        {
+            var users = sqlExecutor.ExecuteReader<User>("SELECT * FROM USERS");
+
+            Assert.NotNull(users);
+            Assert.IsAssignableFrom<IEnumerable<User>>(users);
+        }
+
+        public class User
+        {
+            public string Id { get; set; }
+            public string Email { get; set; }
+            public int Age { get; set; }
+        }
+    }
+}
