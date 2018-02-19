6d52cde8e7d92b0a0d9852eb783c63610f3e2ce9
Seymour Poler <seymourpoler@gmail.com>
2018-02-19 15:00:16 +0100

    green  DeleteAll()

---
+++ Gambon/Gambon/src/Gambon/DeleteCommand.cs
@@ -0,0 +1,22 @@
+﻿using Gambon.Sql;
+using System;
+
+namespace Gambon
+{
+    public class DeleteCommand
+    {
+        private readonly ISqlExecutorWithGeneric sqlExecutor;
+        private readonly ISqlBuilder sqlBuilder;
+
+        public DeleteCommand(ISqlExecutorWithGeneric sqlExecutor, ISqlBuilder sqlBuilder)
+        {
+            this.sqlExecutor = sqlExecutor;
+            this.sqlBuilder = sqlBuilder;
+        }
+
+        public void Execute<T>(dynamic condition = null) where T : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Gambon/Gambon/test/unit/GambonUnitTest/DeleteCommandTests.cs
@@ -0,0 +1,39 @@
+﻿using Gambon.Sql;
+using Moq;
+using Xunit;
+
+namespace Gambon.Test.Unit
+{
+    public class DeleteCommandTests
+    {
+        private readonly Mock<ISqlExecutorWithGeneric> sqlExecutor;
+        private readonly ISqlBuilder sqlBuilder;
+        DeleteCommand command;
+
+        public DeleteCommandTests()
+        {
+            sqlExecutor = new Mock<ISqlExecutorWithGeneric>();
+            sqlBuilder = new SqlBuilder();
+            command = new DeleteCommand(
+                sqlBuilder: sqlBuilder,
+                sqlExecutor: sqlExecutor.Object);
+        }
+
+        [Fact]
+        public void DeleteAll()
+        {
+            command.Execute<User>();
+
+            sqlExecutor
+                .Verify(x => x.ExecuteReader<User>(It.Is<string>(y =>
+                    y.Contains("DELETE * FROM Users"))));
+        }
+
+        public class User
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+    }
+}
