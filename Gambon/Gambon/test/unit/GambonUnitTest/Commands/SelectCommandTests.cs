b5b3c49227af661d9aa952cf2abd4a1f23bf9b2f
Seymour Poler <seymourpoler@gmail.com>
2018-02-21 14:42:37 +0100

    remove duplicated class

--- Gambon/Gambon/test/unit/GambonUnitTest/Commands/SelectCommandTests.cs
+++ Gambon/Gambon/test/unit/GambonUnitTest/Commands/SelectCommandTests.cs
@@ -46,14 +46,5 @@ namespace Gambon.Test.Unit.Commands
             sqlExecutor
                 .Verify(x => x.ExecuteReader<User>(It.Is<string>(y => y.Contains(" WHERE Age = 12"))));
         }
-
-        public class User
-        {
-            public string Id { get; set; }
-            public string Email { get; set; }
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-            public int Age { get; set; }
-        }
     }
 }
