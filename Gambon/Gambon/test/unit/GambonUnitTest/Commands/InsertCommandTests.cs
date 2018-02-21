fb4d4a195af4fbeea6b8e8f5f48da315dcaafb90
Seymour Poler <seymourpoler@gmail.com>
2018-02-21 14:50:43 +0100

    green InsertsUserWithCondition

--- Gambon/Gambon/test/unit/GambonUnitTest/Commands/InsertCommandTests.cs
+++ Gambon/Gambon/test/unit/GambonUnitTest/Commands/InsertCommandTests.cs
@@ -7,20 +7,37 @@ namespace Gambon.Test.Unit.Commands
 {
     public class InsertCommandTests
     {
+        private readonly SqlBuilder sqlBuilder;
+        private readonly Mock<ISqlExecutorWithGeneric> sqlExecutor;
+        private readonly InsertCommand command;
+        private readonly User user;
+
+        public InsertCommandTests()
+        {
+            sqlBuilder = new SqlBuilder();
+            sqlExecutor = new Mock<ISqlExecutorWithGeneric>();
+            command = new InsertCommand(
+                sqlBuilder: sqlBuilder,
+                sqlExecutor: sqlExecutor.Object);
+            user = new User { Id = 12, Age = 34, Name = "John" };
+        }
+
         [Fact]
         public void InsertsUser()
         {
-            var sqlBuilder = new SqlBuilder();
-            var sqlExecutor = new Mock<ISqlExecutorWithGeneric>();
-            var command = new InsertCommand(
-                sqlBuilder: sqlBuilder,
-                sqlExecutor: sqlExecutor.Object);
-            var user = new User { };
-
             command.Execute(entity: user);
 
             sqlExecutor
                 .Verify(x => x.ExecuteNonQuery(It.Is<string>(y => y.Contains("INSERT INTO Users"))));
+        }
+
+        [Fact]
+        public void InsertsUserWithCondition()
+        {
+            command.Execute(entity: user, condition: new { Email = "a@a.es", Age = 24 });
+
+            sqlExecutor
+                .Verify(x => x.ExecuteNonQuery(It.Is<string>(y => y.Contains("WHERE Email = 'a@a.es' AND Age = 24"))));
         }
     }
 }
