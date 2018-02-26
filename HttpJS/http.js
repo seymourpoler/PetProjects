85e00de3b9f7bea8dd85ce0cb2b0d1cb59c7b509
Seymour Poler <seymourpoler@gmail.com>
2018-02-26 19:51:03 +0100

    implementing onreadystatechange

--- HttpJS/http.js
+++ HttpJS/http.js
@@ -4,6 +4,13 @@ function Http(){
     self.get = function(request, successHandler, errorHandler){
         var xmlHttpRequest = new XMLHttpRequest();
         xmlHttpRequest.setRequestHeader("Content-type", "application/json; charset=utf-8");
+        xmlHttpRequest.onreadystatechange = function ParseResult(response) {
+            if (response.readyState == 4 && response.status == 200) {
+                successHandler(response);
+                return;
+            }
+            errorHandler(response);
+        }
         throw 'not implemented';
     };
 }
