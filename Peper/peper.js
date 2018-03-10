c0357ead5da98a4624046bcd43dd064167ef90b9
Seymour Poler <seymourpoler@gmail.com>
2018-03-10 23:09:10 +0100

    start Peper widgets

---
+++ Peper/peper.js
@@ -0,0 +1,142 @@
+(function(peper){
+    peper.Label = function(id){
+        var self = this;
+        var control = document.getElementById(id);
+
+        self.setText = function(text){
+            control.innerHTML = text;
+        };
+
+        self.getText = function(){
+            return control.innerHTML;
+        };
+
+        self.clear = function(){
+            control.innerHTML = '';
+        };
+
+        self.show = function(){
+            control.style.display = 'inline';
+            //control.style.visibility='visible';
+        };
+
+        self.hide = function(){
+            control.style.display = 'none';
+            //control.style.visibility='hidden';
+        };
+    };
+
+    peper.InputText = function(id){
+        var self = this;
+        var control = document.getElementById(id);
+
+        self.setText = function(text){
+            control.value = text;
+        };
+
+        self.getText = function(){
+            return control.value;
+        };
+
+        self.clear = function(){
+            control.value = '';
+        };
+
+        self.show = function(){
+            control.style.display = 'inline';
+            //control.style.visibility='visible';
+        };
+
+        self.hide = function(){
+            control.style.display = 'none';
+            //control.style.visibility='hidden';
+        };
+    };
+
+    peper.Button = function(id){
+        var self = this;
+        var control = document.getElementById(id);
+
+        self.setText = function(text){
+            control.value = text;
+        };
+
+        self.getText = function(){
+            return control.value;
+        };
+
+        self.clear = function(){
+            control.value = '';
+        };
+
+        self.show = function(){
+            control.style.display = 'inline';
+            //control.style.visibility='visible';
+        };
+
+        self.hide = function(){
+            control.style.display = 'none';
+            //control.style.visibility='hidden';
+        };
+    };
+
+    peper.Select = function(id){
+        var self = this;
+        var control = document.getElementById(id);
+
+        self.show = function(){
+            control.style.display = 'inline';
+            //control.style.visibility='visible';
+        };
+
+        self.hide = function(){
+            control.style.display = 'none';
+            //control.style.visibility='hidden';
+        };
+
+        self.setSelectedIndex = function(index){
+            throw 'not implemented';
+        };
+
+        self.getSelectedIndex = function(){
+            throw 'not implemented';
+        };
+
+        self.setSelectedValue = function(value){
+            throw 'not implemented';
+        };
+
+        self.getSelectedValue = function(){
+            throw 'not implemented';
+        };
+
+        self.setSelectedText = function(text){
+            throw 'not implemented';
+        };
+
+        self.getSelectedText = function(){
+            throw 'not implemented';
+        };
+
+        self.add = function(value, text){
+            throw 'not implemented';
+        };
+
+        self.removeByIndex = function(index){
+            throw 'not implemented';
+        };
+
+        self.removeByValue = function(value){
+            throw 'not implemented';
+        };
+
+        self.removeByText = function(text){
+            throw 'not implemented';
+        };
+
+        self.clear = function(){
+            throw 'not implemented';
+        };
+    };
+
+})(window.peper || {})
