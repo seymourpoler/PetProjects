8ed87000fedb02279124f0b1baf28ea503618089
Seymour Poler <seymourpoler@gmail.com>
2018-03-20 22:09:59 +0100

    ignore: format

--- SQUEL/crystal/squel/test/squel/select_test.cr
+++ SQUEL/crystal/squel/test/squel/select_test.cr
@@ -11,76 +11,91 @@ require "../../src/squel/group_builder.cr"
 class SelectTest < Minitest::Test
     def test_select_all_from_table
         sqlSelect = SQUEL::Select.new
+
         assert_equal "SELECT * FROM students", sqlSelect.from("students").to_string
     end
 
     def test_select_one_field_from_table
         sqlSelect = SQUEL::Select.new
+        
         assert_equal "SELECT id FROM students", sqlSelect.field("id").from("students").to_string
     end
 
     def test_select_distinct_field_from_table
         sqlSelect = SQUEL::Select.new
+        
         assert_equal "SELECT DISTINCT id FROM students", sqlSelect.field("id").distinct().from("students").to_string
     end
 
     def test_select_fields_from_table
         sqlSelect = SQUEL::Select.new
+        
         assert_equal "SELECT id, name FROM students", sqlSelect.field("id").field("name").from("students").to_string
     end
 
     def test_select_all_fields_from_tables
         sqlSelect = SQUEL::Select.new
+        
         assert_equal "SELECT * FROM students, admins ad, users", sqlSelect.from("students").from("admins", "ad").from("users").to_string
     end
 
     def test_select_fields_from_table_with_acronimus
         sqlSelect = SQUEL::Select.new
+       
         assert_equal "SELECT id, name FROM students s", sqlSelect.field("id").field("name").from("students", "s").to_string
     end
 
     def test_select_fields_from_table_with_acronimus_with_limit
         sqlSelect = SQUEL::Select.new
+        
         assert_equal "SELECT id, name FROM students s LIMIT 2", sqlSelect.field("id").field("name").from("students", "s").limit(2).to_string
     end
 
     def test_select_fields_from_table_with_offset
         sqlSelect = SQUEL::Select.new
+        
         assert_equal "SELECT id, name FROM students OFFSET 102", sqlSelect.field("id").field("name").from("students").offset(102).to_string
     end
 
     def test_select_fields_from_table_order_by
         sqlSelect = SQUEL::Select.new
+        
         assert_equal "SELECT id, name FROM students ORDER BY age", sqlSelect.field("id").field("name").from("students").order_by("age").to_string
     end
 
     def test_select_fields_from_table_order_by_ascending
         sqlSelect = SQUEL::Select.new
+        
         assert_equal "SELECT name FROM students ORDER BY age ASC", sqlSelect.field("name").from("students").order_by("age").asc().to_string
     end
 
     def test_select_fields_from_table_order_by_descending
         sqlSelect = SQUEL::Select.new
+        
         assert_equal "SELECT name FROM students ORDER BY age DESC", sqlSelect.field("name").from("students").order_by("age").desc().to_string
     end
 
     def test_select_fields_from_table_order_by_descending_and_ascending
         sqlSelect = SQUEL::Select.new
+        
         assert_equal "SELECT id, name FROM students ORDER BY id DESC, name ASC", sqlSelect.field("id").field("name").from("students").order_by("id").desc().order_by("name").asc().to_string
     end
 
     def test_select_fields_from_table_group_by
         sqlSelect = SQUEL::Select.new
+        
         assert_equal "SELECT id, name FROM students GROUP BY email", sqlSelect.field("id").field("name").from("students").group("email").to_string
     end
 
     def test_select_fields_from_table_with_condition
         sqlSelect = SQUEL::Select.new
+        
         assert_equal "SELECT id, name FROM students WHERE email = 'a@a.es'", sqlSelect.field("id").field("name").from("students").where("email = 'a@a.es'").to_string
     end
 
     def test_select_fields_from_table_with_conditions
         sqlSelect = SQUEL::Select.new
+        
         assert_equal "SELECT id, name FROM students WHERE email = 'a@a.es' AND age > 18", sqlSelect.field("id").field("name").from("students").where("email = 'a@a.es'").where("age > 18").to_string
     end
 end
