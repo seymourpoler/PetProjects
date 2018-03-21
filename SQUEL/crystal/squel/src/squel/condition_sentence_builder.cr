f10ac18a00bdd51289cf33a4becc2de4b7cd5662
Seymour Poler <seymourpoler@gmail.com>
2018-03-21 22:52:04 +0100

    green test_expression_with_combination_of_conditions

--- SQUEL/crystal/squel/src/squel/condition_sentence_builder.cr
+++ SQUEL/crystal/squel/src/squel/condition_sentence_builder.cr
@@ -1,23 +1,49 @@
 module SQUEL
     class ConditionSentenceBuilder
         @sentenceResult : String
+        @conditional_before : String
+
+        AND = " AND "
+        OR = " OR "
 
         def initialize
             @sentenceResult = ""
+            @conditional_before = ""
         end
         
         def and(condition : String)
-            if @sentenceResult.empty?
+            if @sentenceResult.empty? || @conditional_before.empty?
                 @sentenceResult = condition
             else
-                @sentenceResult = @sentenceResult + " AND "  + condition
+                @sentenceResult = @sentenceResult + AND  + condition
             end
+            @conditional_before = AND
             return self
         end
 
         def or(condition : String)
-            @sentenceResult = @sentenceResult + " OR "  + condition
+            if @conditional_before.empty?
+                @sentenceResult = @sentenceResult + condition                
+            else
+                @sentenceResult = @sentenceResult + OR  + condition
+            end
+            
+            @conditional_before = OR
             return self
+        end
+
+        def or_begin()
+            if @conditional_before == AND
+                @sentenceResult = @sentenceResult + AND + "("
+            else
+                @sentenceResult = @sentenceResult + OR + "("
+            end
+            @conditional_before = ""
+            return self
+        end
+
+        def end()
+            @sentenceResult = @sentenceResult + ")"
         end
 
         def to_string : String
