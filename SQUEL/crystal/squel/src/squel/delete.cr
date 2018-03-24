b4bd91ae681b250825bd1de68d5de5dcc815d585
Seymour Poler <seymourpoler@gmail.com>
2018-03-24 15:54:18 +0100

    green test_delete_from_table_with_conditions

--- SQUEL/crystal/squel/src/squel/delete.cr
+++ SQUEL/crystal/squel/src/squel/delete.cr
@@ -1,9 +1,11 @@
 module SQUEL
     class Delete
         @table : String
+        @conditions : Array(String)
 
         def initialize
             @table = ""
+            @conditions = [] of String
         end
 
         def from(table : String)
@@ -11,9 +13,20 @@ module SQUEL
             return self
         end
 
+        def where(condition : String)
+            @conditions << "(" + condition + ")"
+            return self
+        end
+
         def to_string
-            return "DELETE FROM " + @table
+            return "DELETE FROM " + @table + build_conditions
+        end
+
+        private def build_conditions
+            if @conditions.empty?
+                return ""
+            end
+            return " WHERE " + StringUtil.join(@conditions, " AND ")
         end
     end
-    
 end
