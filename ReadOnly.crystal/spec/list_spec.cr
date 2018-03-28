bc2cb54817a439dfae3183f4db663720a888088f
Seymour Poler <seymourpoler@gmail.com>
2018-03-28 23:06:28 +0200

    green ReadOnly::List when adding new element, returns the number of elements

--- ReadOnly.crystal/spec/list_spec.cr
+++ ReadOnly.crystal/spec/list_spec.cr
@@ -2,6 +2,14 @@ require "./spec_helper"
 
 describe ReadOnly::List do
     describe "when adding new element" do
+        it "returns the number of elements" do
+            list = ReadOnly::List(Int32).new
+            
+            result = list.add(3).add(4).add(5)
+
+            result.count.should eq 3
+        end
+
         it "returns list with an added element" do
             list = ReadOnly::List(Int32).new
             
