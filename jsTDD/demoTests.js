72500e2b65622fe102b339f3460c8dba4890a85b
Seymour Poler <seymourpoler@gmail.com>
2018-04-10 16:07:41 +0200

    refactor: rename

--- jsTDD/demoTests.js
+++ jsTDD/demoTests.js
@@ -2,10 +2,10 @@ testCase("demo tests", {
     setUp: function(){
         console.log('setup');
     },
-    "test: with false assertion": function(){
+    "test --> with false assertion": function(){
         assert("is false", true == false);
     },
-    "test: with true assertion": function(){
+    "test --> with true assertion": function(){
         assert("is true", true == true);
     },
     tearDown: function(){
