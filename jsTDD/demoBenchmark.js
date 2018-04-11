6557233a0d91a7e59686b221f48369e093faf0bb
Seymour Poler <seymourpoler@gmail.com>
2018-04-11 15:53:23 +0200

    adding benchmark

---
+++ jsTDD/demoBenchmark.js
@@ -0,0 +1,22 @@
+var loopLength = 500000;
+var array = [];
+for (var i = 0; i < loopLength; i++) {
+    array[i] = "item" + i;
+}
+
+
+function forLoop(){
+    for (var i = 0, item; i < array.length; i++) {
+        item = array[i];
+    }
+}
+
+function forLoopCachedLength() {
+    for (var i = 0, l = array.length, item; i < l; i++) {
+        item = array[i];
+    }
+
+}
+
+runBenchmark("forLoop", forLoop);
+runBenchmark("forLoopCachedLength", forLoopCachedLength);
