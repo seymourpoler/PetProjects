ade700cd0327b0912284954f0e32ec5235e08a3f
seymourpoler <seymourpoler@gmail.com>
2018-07-23 00:05:22 +0200

    jspn serializer

---
+++ WiMi/WiMi.CrossCutting/ISerializer.cs
@@ -0,0 +1,7 @@
+﻿namespace WiMi.CrossCutting
+{
+    public interface ISerializer
+    {
+		string Serializer<T>(T entity) where T : class;
+    }
+}
---
+++ WiMi/WiMi.CrossCutting/JsonSerializer.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace WiMi.CrossCutting
+{
+	public class JsonSerializer : ISerializer
+    {
+		public string Serializer<T>(T entity) where T : class
+		{
+			return JsonConvert.SerializeObject(entity);
+		}
+    }
+}
