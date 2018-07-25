fcabb2938ed7855051ef9937cadc8c29fe463ccc
Seymour Poler <seymourpoler@gmail.com>
2018-07-25 14:38:19 +0100

    green return_error_when_title_is_null

---
+++ WiMi/WiMi.Domain.Unit.Test/PageCreatorTests.cs
@@ -0,0 +1,27 @@
+﻿using Shouldly;
+using System.Linq;
+using WiMi.Domain.Pages;
+using Xunit;
+
+namespace WiMi.Domain.Unit.Test
+{
+    public class PageCreatorTests
+    {
+        IPageCreator creator;
+
+        public PageCreatorTests()
+        {
+            creator = new PageCreator();
+        }
+
+        [Fact]
+        public void return_error_when_title_is_null()
+        {
+            var result = creator.Create(new PageCreationRequest(title: null, body: "a"));
+
+            result.IsOk.ShouldBeFalse();
+            result.Errors.First().FieldId.ShouldBe("Title");
+            result.Errors.First().ErrorCode.ShouldBe(nameof(Error.ErrorCodes.Required));
+        }
+    }
+}
---
+++ WiMi/WiMi.Domain/Pages/PageCreationRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace WiMi.Domain.Pages
+{
+    public class PageCreationRequest
+    {
+        public string Title { get; }
+        public string Body { get; }
+
+        public PageCreationRequest(string title, string body)
+        {
+            Title = title;
+            Body = body;
+        }
+    }
+}
