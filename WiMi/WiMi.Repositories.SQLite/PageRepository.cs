d2958f9cdbd2ddf2cb80fba8279d00e79943e387
seymourpoler <seymourpoler@gmail.com>
2018-07-25 20:47:26 +0200

    adding PageRepository

---
+++ WiMi/WiMi.Repositories.SQLite/PageRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using WiMi.Domain.Pages;
+
+namespace WiMi.Repositories.SQLite
+{
+	public class PageRepository : IPageRepository
+	{
+		void IPageRepository.Save(Page page)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
