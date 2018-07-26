19fd7ee520d1597393f24bed66f014b7a26a81bb
Luis Rodríguez <seymourpoler@hotmail.com>
2018-07-26 15:13:29 +0200

    public

--- WiMi/WiMi.Migrations.SQLite/Program.cs
+++ WiMi/WiMi.Migrations.SQLite/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace WiMi.Migrations.SQLite
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
