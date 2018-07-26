58da7987ab7fb9590f7567c30ad16e953b67dcd4
Luis Rodríguez <seymourpoler@hotmail.com>
2018-07-26 15:13:46 +0200

    adding string connections configuration  class

---
+++ WiMi/WiMi.Repositories.SQLite/RepositoryConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace WiMi.Repositories.SQLite
+{
+    public class RepositoryConfiguration
+    {
+        public string ConnectionString { get { return String.Empty; } }
+    }
+}
