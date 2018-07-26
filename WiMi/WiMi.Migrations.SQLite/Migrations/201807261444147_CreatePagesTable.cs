22f710d40f6d1a027d77423472b3aa4d5caecf05
Luis Rodríguez <seymourpoler@hotmail.com>
2018-07-26 15:05:06 +0200

    adding migrations project

---
+++ WiMi/WiMi.Migrations.SQLite/Migrations/201807261444147_CreatePagesTable.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace WiMi.Migrations.SQLite.Migrations
+{
+    [Migration(201807261444147, nameof(CreatePagesTable))]
+    public class CreatePagesTable : Migration
+    {
+        public override void Up()
+        {
+            Create
+                .Table("Pages")
+                .WithColumn("Id").AsString(256).PrimaryKey().NotNullable()
+                .WithColumn("Title").AsString(256).NotNullable()
+                .WithColumn("Body").AsString(1000).NotNullable()
+                .WithColumn("CreationDate").AsDateTime2().NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Pages");
+        }
+    }
+}
---
+++ WiMi/WiMi.Migrations.SQLite/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WiMi.Migrations.SQLite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = CreateServices();
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+        }
+        static IServiceProvider CreateServices()
+        {
+            return new ServiceCollection()
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    .AddSQLite() //TODO: locate dataBase
+                    .WithGlobalConnectionString("Data Source=WiMiDataBase.db")
+                    .ScanIn(Assembly.GetExecutingAssembly()).For.Migrations())
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                .BuildServiceProvider(false);
+        }
+
+        static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+            runner.MigrateUp();
+        }
+    }
+}
