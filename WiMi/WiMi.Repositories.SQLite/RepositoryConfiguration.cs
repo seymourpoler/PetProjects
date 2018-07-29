df6d0cd6c1bfa774a9447b5d300d61a7de6d4b73
Seymour Poler <seymourpoler@gmail.com>
2018-07-29 15:10:23 +0200

    remove not needed file

--- WiMi/WiMi.Repositories.SQLite/RepositoryConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace WiMi.Repositories.SQLite
-{
-    public class RepositoryConfiguration
-    {
-        public string ConnectionString { get { return String.Empty; } }
-    }
-}
