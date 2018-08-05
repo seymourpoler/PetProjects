1d414f4cb386d423a6a0b5b330d26bc514f956ee
Seymour Poler <seymourpoler@gmail.com>
2018-08-05 13:38:06 +0200

    clean commented code

--- WiMi/src/WiMi.Web/wwwroot/lib/WiMi.js
+++ WiMi/src/WiMi.Web/wwwroot/lib/WiMi.js
@@ -1,26 +1,6 @@
 ﻿window.WiMi = window.WiMi || {};
 
 (function (WiMi) {
-    //function namespace(namespace) {
-    //    const values = namespace.split(".");
-    //    let result = buildNamespace(values);
-    //    const nameClass = Object.keys(result)[0];
-    //    if (!WiMi[nameClass]) {
-    //        WiMi[nameClass] = result[nameClass];
-    //    }
-
-    //    function buildNamespace(values) {
-    //        if (values.length == 0 || values == undefined) {
-    //            return {};
-    //        }
-
-    //        var head = values[0];
-    //        var result = {};
-    //        values.shift()
-    //        result[head] = buildNamespace(values);
-    //        return result;
-    //    }
-    //};
     function namespace(ns) {
         var names = ns.split(".");
         var lastNameSpace = WiMi || {};
