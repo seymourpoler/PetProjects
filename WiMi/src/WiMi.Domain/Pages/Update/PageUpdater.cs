274abf319e1f579da056330d4c9667272ba6989d
Luis Rodríguez <seymourpoler@hotmail.com>
2018-09-07 18:05:19 +0200

    green return_not_found_error_when_page_is_not_found

---
+++ WiMi/src/WiMi.Domain/Pages/Update/PageUpdater.cs
@@ -0,0 +1,22 @@
+﻿namespace WiMi.Domain.Pages.Update
+{
+    public class PageUpdater : IPageUpdater
+    {
+        readonly IExistPageRepository existPageRepository;
+
+        public PageUpdater(
+            IExistPageRepository existPageRepository)
+        {
+            this.existPageRepository = existPageRepository;
+        }
+
+        public ServiceExecutionResult Update(PageUpdatingRequest request)
+        {
+            if(!existPageRepository.Exist(request.Id))
+            {
+                return new ServiceExecutionResult(new Error(Error.ErrorCodes.NotFound));
+            }
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ WiMi/test/WiMi.Domain.Unit.Test/Pages/PageUpdaterTests.cs
@@ -0,0 +1,42 @@
+﻿using Moq;
+using Shouldly;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using WiMi.Domain.Pages;
+using WiMi.Domain.Pages.Update;
+using Xunit;
+
+namespace WiMi.Domain.Unit.Test.Pages
+{
+    public class PageUpdaterTests
+    {
+        Mock<IExistPageRepository> existPageRepository;
+        IPageUpdater updater;
+
+        public PageUpdaterTests()
+        {
+            existPageRepository = new Mock<IExistPageRepository>();
+            updater = new PageUpdater(
+                existPageRepository: existPageRepository.Object);
+        }
+
+        [Fact]
+        public async Task return_not_found_error_when_page_is_not_found()
+        {
+            var id = Guid.NewGuid();
+            existPageRepository
+                .Setup(x => x.Exist(id))
+                .Returns(false);
+
+            var result = updater.Update(new PageUpdatingRequest(
+                id: id,
+                title: "title",
+                body: "body"));
+
+            result.IsOk.ShouldBeFalse();
+            result.Errors.First().FieldId.ShouldBe(Error.GeneralFieldIdError);
+            result.Errors.First().ErrorCode.ShouldBe(nameof(Error.ErrorCodes.NotFound));
+        }
+    }
+}
