b084c7f3d7fa7609e9a4356577a901afdf8c95e3
Luis Rodríguez <seymourpoler@hotmail.com>
2018-09-07 06:26:24 +0200

    implementing RemovePageRepository

---
+++ WiMi/src/WiMi.Repositories.SQLite/RemovePageRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SQLite;
+using WiMi.Domain.Pages.Remove;
+
+namespace WiMi.Repositories.SQLite
+{
+    public class RemovePageRepository : IRemovePageRepository
+    {
+        readonly DataBaseConfiguration configuration;
+
+        public RemovePageRepository(DataBaseConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void Remove(Guid id)
+        {
+            using (var connection = new SQLiteConnection(configuration.ConnectionString))
+            {
+                connection.Open();
+                var sql = $"DELETE FROM Pages WHERE Id = '{id.ToString()}'";
+                using (var command = new SQLiteCommand(commandText: sql, connection: connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+                connection.Close();
+            }
+        }
+    }
+}
