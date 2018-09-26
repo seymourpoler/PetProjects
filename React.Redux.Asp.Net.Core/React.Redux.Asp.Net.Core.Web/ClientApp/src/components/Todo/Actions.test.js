70c9f36637880b72d470ebbf311b74793db223fd
Luis Rodríguez <seymourpoler@hotmail.com>
2018-09-26 07:21:38 +0200

    green when action requested, adds todo item

--- React.Redux.Asp.Net.Core/React.Redux.Asp.Net.Core.Web/ClientApp/src/components/Todo/Actions.test.js
+++ React.Redux.Asp.Net.Core/React.Redux.Asp.Net.Core.Web/ClientApp/src/components/Todo/Actions.test.js
@@ -1,12 +1,23 @@
 ﻿import { REMOVE_TODO_ITEM, ADD_TODO_ITEM } from './Actions.Types';
 import { removeTodo, addTodo } from './Actions';
 
-describe('when remove todo is requested', () => {
+describe('when action requested', () => {
     it('removes todo item', () => {
         const id = 1;
+
         let result = removeTodo(id);
 
         expect(result.type).toBe(REMOVE_TODO_ITEM);
         expect(result.payload).toBe(id);
     });
+
+    it('adds todo item', () => {
+        const item = { id: 1, title: 'title', description: 'description' };
+
+        let result = addTodo(item);
+
+        expect(result.type).toBe(ADD_TODO_ITEM);
+        expect(result.payload).toBe(item);
+    });
 });
+
