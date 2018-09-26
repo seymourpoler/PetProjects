1107bfc319fc18c4df9e6cf1870412f35384bda5
Luis Rodríguez <seymourpoler@hotmail.com>
2018-09-26 16:34:07 +0200

    adding spinner component

---
+++ React.Redux.Asp.Net.Core/React.Redux.Asp.Net.Core.Web/ClientApp/src/components/Spinner.js
@@ -0,0 +1,15 @@
+﻿import React, { Component } from 'react';
+
+export default class Spinner extends Component {
+    render() {
+        let result = '';
+        if (this.props.show) {
+            result = < p > loading ... </p >;
+        }
+        return (
+            <div>
+                { result }
+            </div>
+            );
+    }
+}
