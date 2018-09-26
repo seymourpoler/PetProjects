3d9401d303bea1232592bb34fb0b199f7f8f7f01
Luis Rodríguez <seymourpoler@hotmail.com>
2018-09-26 07:32:52 +0200

    green when todo items reducing is requested, add new todo item

---
+++ React.Redux.Asp.Net.Core/React.Redux.Asp.Net.Core.Web/ClientApp/src/components/Todo/Reducer.test.js
@@ -0,0 +1,15 @@
+﻿import reducer from './Reducer';
+import { ADD_TODO_ITEM } from './Actions.Types';
+
+describe('when todo items reducing is requested', () => {
+    it('add new todo item', () => {
+        const item = { id: 1, title: 'title', description: 'description' };
+        const action = { type: ADD_TODO_ITEM, payload: item };
+        const state = { todos: [] };
+
+        let result = reducer(state, action);
+
+        expect(result.todos.length).toBe(1);
+        expect(result.todos[0].title).toBe(item.title);
+    });
+});
