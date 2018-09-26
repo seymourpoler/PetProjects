7f9e4dafcb2220c0ac7f8e44733229d17e04ce9d
Luis Rodríguez <seymourpoler@hotmail.com>
2018-09-26 07:47:54 +0200

    green when todo items reducing is requested, removes new todo item

--- React.Redux.Asp.Net.Core/React.Redux.Asp.Net.Core.Web/ClientApp/src/components/Todo/Reducer.test.js
+++ React.Redux.Asp.Net.Core/React.Redux.Asp.Net.Core.Web/ClientApp/src/components/Todo/Reducer.test.js
@@ -1,5 +1,5 @@
 ﻿import reducer from './Reducer';
-import { ADD_TODO_ITEM } from './Actions.Types';
+import { REMOVE_TODO_ITEM, ADD_TODO_ITEM } from './Actions.Types';
 
 describe('when todo items reducing is requested', () => {
     it('adds new todo item', () => {
@@ -11,5 +11,15 @@ describe('when todo items reducing is requested', () => {
 
         expect(result.todos.length).toBe(1);
         expect(result.todos[0].title).toBe(item.title);
+    });
+
+    it('removes new todo item', () => {
+        const item = { id: 1, title: 'title', description: 'description' };
+        const action = { type: REMOVE_TODO_ITEM, payload: item.id };
+        const state = { todos: [item] };
+
+        let result = reducer(state, action);
+
+        expect(result.todos.length).toBe(0);
     });
 });
