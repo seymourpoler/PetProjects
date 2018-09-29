64b194ee7d5abe50bb3d631d39241d61bed00120
Luis Rodríguez <seymourpoler@hotmail.com>
2018-09-29 23:49:08 +0200

    green initializes state if is undefined

---
+++ React.Redux.Asp.Net.Core/React.Redux.Asp.Net.Core.Web/ClientApp/src/components/Articles/Reducer.js
@@ -0,0 +1,17 @@
+﻿import { SHOW_SPINNER, HIDE_SPINNER, INTERNAL_SERVER_ERROR, BAD_REQUEST, SHOW_ARTICLES } from './Actions.types';
+import HttpStatusCode from '../../HttpStatusCode';
+
+const initialState = { statusCode: HttpStatusCode.Ok, articles: [] };
+
+export default function reducer(state, action) {
+    state = state || initialState;
+    switch (action.type) {
+        case SHOW_SPINNER:
+        case HIDE_SPINNER:
+        case INTERNAL_SERVER_ERROR:
+            throw 'not implemented';
+        
+        default:
+            return state;
+    }
+}
---
+++ React.Redux.Asp.Net.Core/React.Redux.Asp.Net.Core.Web/ClientApp/src/components/Articles/Reducer.test.js
@@ -0,0 +1,16 @@
+﻿import reducer from './Reducer';
+import HttpStatusCode from '../../HttpStatusCode';
+
+describe('Reducer', () => {
+    it('initializes state if is undefined', () => {
+        const result = reducer(undefined, {});
+
+        expect(result.articles).not.toBeNull();
+        expect(result.statusCode).toBe(HttpStatusCode.Ok);
+    });
+
+    xit('returns state for showing spinner', () => {
+        const result = reducer({});
+        
+    });
+});
