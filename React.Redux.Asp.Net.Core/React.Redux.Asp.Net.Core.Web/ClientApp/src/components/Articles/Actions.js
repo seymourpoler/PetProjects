f7b1a1040549b1e4b72bc30cb5c8911d20b20197
Luis Rodríguez <seymourpoler@hotmail.com>
2018-09-29 23:32:53 +0200

    green when finding articles is requested, shows articles

--- React.Redux.Asp.Net.Core/React.Redux.Asp.Net.Core.Web/ClientApp/src/components/Articles/Actions.js
+++ React.Redux.Asp.Net.Core/React.Redux.Asp.Net.Core.Web/ClientApp/src/components/Articles/Actions.js
@@ -1,4 +1,4 @@
-﻿import { SHOW_SPINNER, HIDE_SPINNER, INTERNAL_SERVER_ERROR, BAD_REQUEST } from './Actions.types';
+﻿import { SHOW_SPINNER, HIDE_SPINNER, INTERNAL_SERVER_ERROR, BAD_REQUEST, SHOW_ARTICLES } from './Actions.types';
 import Service from './Service';
 import HttpStatusCode from '../../HttpStatusCode';
 
@@ -23,5 +23,5 @@ export const findArticles = () => {
         return { type: BAD_REQUEST, errors: result.errors };
     }
 
-    throw 'not implemented';
+    return { type: SHOW_ARTICLES, articles: result.articles };
 };
--- React.Redux.Asp.Net.Core/React.Redux.Asp.Net.Core.Web/ClientApp/src/components/Articles/Actions.test.js
+++ React.Redux.Asp.Net.Core/React.Redux.Asp.Net.Core.Web/ClientApp/src/components/Articles/Actions.test.js
@@ -1,4 +1,4 @@
-﻿import { SHOW_SPINNER, HIDE_SPINNER, INTERNAL_SERVER_ERROR, BAD_REQUEST } from './Actions.types';
+﻿import { SHOW_SPINNER, HIDE_SPINNER, INTERNAL_SERVER_ERROR, BAD_REQUEST, SHOW_ARTICLES } from './Actions.types';
 import { showSpinner, hideSpinner, findArticles } from './Actions';
 import Service from './Service';
 import HttpStatusCode from '../../HttpStatusCode';
@@ -36,6 +36,17 @@ describe('Articles', () => {
 
             expect(result.type).toBe(BAD_REQUEST);
             expect(result.errors).not.toBeNull();
+        });
+
+        it('shows articles', () => {
+            Service.find = () => {
+                return { statusCode: HttpStatusCode.Ok, articles: [{ id:1, title: 'title-article', description: 'description-article' }] };
+            };
+
+            let result = findArticles();
+
+            expect(result.type).toBe(SHOW_ARTICLES);
+            expect(result.articles).not.toBeNull();
         });
     });
 });
