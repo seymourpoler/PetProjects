3c9f09daddb76e1107e3de9bf27526ccc8ba9d3e
Luis Rodríguez <seymourpoler@hotmail.com>
2018-10-01 23:32:43 +0200

    green returns internal server error if there is internal server error

--- React.Redux.Asp.Net.Core/React.Redux.Asp.Net.Core.Web/ClientApp/src/components/Articles/Service.js
+++ React.Redux.Asp.Net.Core/React.Redux.Asp.Net.Core.Web/ClientApp/src/components/Articles/Service.js
@@ -1,13 +1,25 @@
-﻿export default class Service {
-    find() {
+﻿import { INTERNAL_SERVER_ERROR } from '../../HttpStatusCode.types';
+
+export default class Service {
+    async find () {
+        const response = await fetch('/api/articles', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        console.log(response)
+        if (response.status === INTERNAL_SERVER_ERROR) {
+            return { type: INTERNAL_SERVER_ERROR };
+        }
         throw 'not implemented';
     }
 
-    save(article) {
+    save (article) {
         throw 'not implemented';
     }
 
-    delete(id) {
+    delete (id) {
         throw 'not implemented';
     }
 }
---
+++ React.Redux.Asp.Net.Core/React.Redux.Asp.Net.Core.Web/ClientApp/src/components/Articles/Service.test.js
@@ -0,0 +1,20 @@
+﻿import Service from './Service';
+import { INTERNAL_SERVER_ERROR } from '../../HttpStatusCode.types';
+
+describe('Service', () => {
+    let service;
+
+    beforeEach(() => {
+        service = new Service();
+    });
+
+    it('returns internal server error if there is internal server error', async () => {
+        window.fetch = jest.fn().mockImplementation(() => ({
+            status: INTERNAL_SERVER_ERROR,
+        }));
+
+        const result = await service.find();
+
+        expect(result.type).toBe(INTERNAL_SERVER_ERROR);
+    });
+});
