2f91f27da68fc9ff2d19a3d5f69f2c4e69f36772
Luis Rodríguez <seymourpoler@hotmail.com>
2018-10-03 15:22:42 +0200

    show errors

--- React.Redux.Asp.Net.Core/React.Redux.Asp.Net.Core.Web/ClientApp/src/components/Articles/List.js
+++ React.Redux.Asp.Net.Core/React.Redux.Asp.Net.Core.Web/ClientApp/src/components/Articles/List.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import Spinner from '../Spinner';
 import Service from './Service';
 import { showSpinner, hideSpinner } from './Actions';
+import { OK, INTERNAL_SERVER_ERROR, BAD_REQUEST } from '../../HttpStatusCode.types';
 
 class List extends Component {
 
@@ -24,12 +25,20 @@ class List extends Component {
     };
 
     render() {
-        let result = 'No resuts';
-        if (this.props.articles && Array.isArray(this.props.articles)) {
+        let result = 'No results';
+        if (this.props.statusCode === INTERNAL_SERVER_ERROR) {
+            result = 'There is an internal server error';
+        }
+        if (this.props.statusCode === BAD_REQUEST && this.props.errors && Array.isArray(this.props.errors)) {
+            result = this.props.errors.map((error, index) => {
+                return (<p key={index}> {error.fieldId} : {error.errorCode} </p>);
+            }); 
+        }
+        else if (this.props.statusCode === OK && this.props.articles && Array.isArray(this.props.articles)) {
             result = this.props.articles.map((article, index) => {
                 return (<a key={index} id={article.id} onClick={this.removeItem}> {article.title} </a>);
-        });
-    }
+            });
+        }
 
     return (
             <div>
