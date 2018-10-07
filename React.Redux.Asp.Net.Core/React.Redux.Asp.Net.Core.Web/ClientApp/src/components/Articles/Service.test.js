6b9d39f09f2d2296ac26e16e945161e4bdfd6d1d
Luis Rodríguez <seymourpoler@hotmail.com>
2018-10-07 16:30:13 +0200

    grouping test in the same describe

--- React.Redux.Asp.Net.Core/React.Redux.Asp.Net.Core.Web/ClientApp/src/components/Articles/Service.test.js
+++ React.Redux.Asp.Net.Core/React.Redux.Asp.Net.Core.Web/ClientApp/src/components/Articles/Service.test.js
@@ -4,47 +4,49 @@ import HttpStatusCode from '../../HttpStatusCode';
 describe('Service', () => {
     let service;
 
-    beforeEach(() => {
-        service = new Service();
-    });
+    describe('when finding articles is requested', () => {
+        beforeEach(() => {
+            service = new Service();
+        });
 
-    it('returns internal server error if there is internal server error', async () => {
-        window.fetch = jest.fn().mockImplementation(() => ({
-            status: HttpStatusCode.InternalServerError
-        }));
+        it('returns internal server error if there is internal server error', async () => {
+            window.fetch = jest.fn().mockImplementation(() => ({
+                status: HttpStatusCode.InternalServerError
+            }));
 
-        const result = await service.find();
+            const result = await service.find();
 
-        expect(result.type).toBe(HttpStatusCode.InternalServerError);
-    });
+            expect(result.type).toBe(HttpStatusCode.InternalServerError);
+        });
 
-    it('returns bad request if there errors', async () => {
-        const errors = [{ fieldId: 'title', errorCode: 'Required' }, { fieldId: 'subTitle', errorCode: 'Required' }];
-        window.fetch = jest.fn().mockImplementation(() => ({
-            status: HttpStatusCode.BadRequest,
-            json: () => new Promise((resolve, reject) => {
-                resolve(errors);
-            })
-        }));
+        it('returns bad request if there errors', async () => {
+            const errors = [{ fieldId: 'title', errorCode: 'Required' }, { fieldId: 'subTitle', errorCode: 'Required' }];
+            window.fetch = jest.fn().mockImplementation(() => ({
+                status: HttpStatusCode.BadRequest,
+                json: () => new Promise((resolve, reject) => {
+                    resolve(errors);
+                })
+            }));
 
-        const result = await service.find();
+            const result = await service.find();
 
-        expect(result.type).toBe(HttpStatusCode.BadRequest);
-        expect(result.errors).toBe(errors);
-    });
+            expect(result.type).toBe(HttpStatusCode.BadRequest);
+            expect(result.errors).toBe(errors);
+        });
 
-    it('returns articles', async () => {
-        const articles = [{ id: 1, title: 'title-one' }, { id: 2, title: 'title-two' }];
-        window.fetch = jest.fn().mockImplementation(() => ({
-            status: HttpStatusCode.Ok,
-            json: () => new Promise((resolve, reject) => {
-                resolve(articles);
-            })
-        }));
+        it('returns articles', async () => {
+            const articles = [{ id: 1, title: 'title-one' }, { id: 2, title: 'title-two' }];
+            window.fetch = jest.fn().mockImplementation(() => ({
+                status: HttpStatusCode.Ok,
+                json: () => new Promise((resolve, reject) => {
+                    resolve(articles);
+                })
+            }));
 
-        const result = await service.find();
+            const result = await service.find();
 
-        expect(result.type).toBe(HttpStatusCode.Ok);
-        expect(result.articles).toBe(articles);
+            expect(result.type).toBe(HttpStatusCode.Ok);
+            expect(result.articles).toBe(articles);
+        });
     });
 });
