9cebc578e02d077386bd621136b1321de19ad4e1
Seymour Poler <seymourpoler@gmail.com>
2018-10-10 04:59:08 +0200

    refactor: extract class

---
+++ React.Redux.Asp.Net.Core/React.Redux.Asp.Net.Core.Web/ClientApp/src/http.js
@@ -0,0 +1,28 @@
+﻿export default class Http {
+    headers = { 'Content-Type': 'application/json' };
+
+    async get(url) {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: headers
+        });
+        return response;
+    }
+
+    async post(url, data) {
+        const response = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: headers
+        });
+        return response;
+    }
+
+    async delete(url) {
+        const response = await fetch(url, {
+            method: 'DELETE',
+            headers: headers
+        });
+        return response;
+    }
+}
