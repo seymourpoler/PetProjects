dd4f48eb9ea0e120c3bf39085861723705f7574f
Seymour Poler <seymourpoler@gmail.com>
2018-12-07 14:55:58 +0100

    using closure with Span control

--- UOS/src/span.js
+++ UOS/src/span.js
@@ -1,30 +1,32 @@
-UOS.Span = function Span(configuration){
-    var self = this;
-
-    self.removeCssClass = function(cssClass){
-        throw 'not implemented';
-    };
-
-    self.render = function(){
-        if(!configuration){
-            return '<span></span>';
-        }
-        let result = '<span';
-        if(configuration.id){
-            result = result + " id='" + configuration.id + "'";
-        }
-        if(configuration.name){
-            result = result + " name='" + configuration.name + "'";
-        }
-        if(configuration.style){
-            result = result + " style='" + configuration.style + "'";
-        }
-
-        if(configuration.cssClass){
-            result = result + " class='" + configuration.cssClass + "'";
-        }
-        
-        result = result + '></span>';
-        return result;
-    };
-}
+(function(UOS){
+    UOS.Span = function Span(configuration){
+        var self = this;
+    
+        self.removeCssClass = function(cssClass){
+            throw 'not implemented';
+        };
+    
+        self.render = function(){
+            if(!configuration){
+                return '<span></span>';
+            }
+            let result = '<span';
+            if(configuration.id){
+                result = result + " id='" + configuration.id + "'";
+            }
+            if(configuration.name){
+                result = result + " name='" + configuration.name + "'";
+            }
+            if(configuration.style){
+                result = result + " style='" + configuration.style + "'";
+            }
+    
+            if(configuration.cssClass){
+                result = result + " class='" + configuration.cssClass + "'";
+            }
+            
+            result = result + '></span>';
+            return result;
+        };
+    }
+})(UOS || {});
