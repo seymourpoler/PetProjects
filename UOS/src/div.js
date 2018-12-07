1618bea10365c69c886821d99fd3a3c5dbb51e30
Seymour Poler <seymourpoler@gmail.com>
2018-12-07 14:54:02 +0100

    using closure with Div control

--- UOS/src/div.js
+++ UOS/src/div.js
@@ -1,26 +1,28 @@
-UOS.Div = function Div(configuration){
-    let self = this;
-
-    self.render = function(){
-        if(!configuration){
-            return '<div></div>';
-        }
-        let result = '<div';
-        if(configuration.id){
-            result = result + " id='" + configuration.id + "'";
-        }
-        if(configuration.name){
-            result = result + " name='" + configuration.name + "'";
-        }
-        if(configuration.style){
-            result = result + " style='" + configuration.style + "'";
-        }
-
-        if(configuration.cssClass){
-            result = result + " class='" + configuration.cssClass + "'";
-        }
-        
-        result = result + '></div>';
-        return result;
-    };
-}
+(function(UOS){
+    UOS.Div = function Div(configuration){
+        let self = this;
+    
+        self.render = function(){
+            if(!configuration){
+                return '<div></div>';
+            }
+            let result = '<div';
+            if(configuration.id){
+                result = result + " id='" + configuration.id + "'";
+            }
+            if(configuration.name){
+                result = result + " name='" + configuration.name + "'";
+            }
+            if(configuration.style){
+                result = result + " style='" + configuration.style + "'";
+            }
+    
+            if(configuration.cssClass){
+                result = result + " class='" + configuration.cssClass + "'";
+            }
+            
+            result = result + '></div>';
+            return result;
+        };
+    }
+})(UOS || {});
