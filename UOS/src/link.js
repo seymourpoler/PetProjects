669ce819a6a48c3f07bc3bbdeb2d621ce9868da2
Seymour Poler <seymourpoler@gmail.com>
2018-12-07 14:55:07 +0100

    using closure with Link control

--- UOS/src/link.js
+++ UOS/src/link.js
@@ -1,15 +1,17 @@
-UOS.Link = function Link(configuration){
-    let self = this;
-
-    self.render = function(){
-        if(!configuration){
-            return '<a></a>';
-        }
-        let result = '<a';
-        if(configuration.id){
-            result = result + " id='" + configuration.id + "'";
-        }
-        result = result + '></a>';
-        return result;
-    };
-}
+(function(UOS){
+    UOS.Link = function Link(configuration){
+        let self = this;
+    
+        self.render = function(){
+            if(!configuration){
+                return '<a></a>';
+            }
+            let result = '<a';
+            if(configuration.id){
+                result = result + " id='" + configuration.id + "'";
+            }
+            result = result + '></a>';
+            return result;
+        };
+    }
+})(UOS || {});
