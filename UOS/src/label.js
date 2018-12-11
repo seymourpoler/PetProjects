ae26e4ae05075b78bc6ed51d5fa52195d71e69f8
Seymour Poler <seymourpoler@gmail.com>
2018-12-11 15:51:59 +0100

    using namespace

--- UOS/src/label.js
+++ UOS/src/label.js
@@ -1,9 +1,24 @@
 (function(UOS){
-    UOS.Label = function Label(){
+    UOS.Label = function Label(controlRenderer, configuration){
         var self = this;
         
         self.render = function(){
-            return '<label></label>';
+            if(!configuration){
+                return '<label></label>';
+            }
+            const result = controlRenderer.render({...configuration, tag: 'label'});
+            return result;
         };
     }
+
+    function createLabel(configuration){
+        return new UOS.Controls.Label(
+            UOS.Controls.createControlRenderer(),
+            configuration);
+    }
+
+    UOS.namespace('Controls');
+    UOS.Controls.createLabel = createLabel;
+    UOS.Controls.label = Label;
+
 })(UOS);
