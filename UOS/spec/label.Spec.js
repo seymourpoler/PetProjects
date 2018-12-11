2bfd80f3a3e937409c1430c7536a67139c95beed
Seymour Poler <seymourpoler@gmail.com>
2018-12-11 16:59:59 +0100

    green Label renders with style

--- UOS/spec/label.Spec.js
+++ UOS/spec/label.Spec.js
@@ -14,4 +14,12 @@ describe('Label', function(){
 
         expect(result).toEqual("<label id='label-id'></label>");
     });
+	
+	it("renders with style", function(){
+        var label = UOS.Controls.createLabel({id:'label-id', style:'color:black'});
+		
+        const result = label.render();
+
+        expect(result).toEqual("<label id='label-id' style='color:black'></label>");
+    });
 });
