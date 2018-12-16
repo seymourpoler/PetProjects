09c74b67d6cb352b015a1d097590c4777b86ac12
seymourpoler <seymourpoler@gmail.com>
2018-12-16 17:03:30 +0100

    dummy, stub and mock, shows a stub

--- dummy-stub-mock/index.js
+++ dummy-stub-mock/index.js
@@ -1,3 +1,7 @@
 function Sut(client){
+	var self = this;
 
+	self.getValue = function(){
+		return client.getValue();
+	};
 }
--- dummy-stub-mock/index.spec.js
+++ dummy-stub-mock/index.spec.js
@@ -1,9 +1,19 @@
 describe('dummy, stub and mock', function(){
-	it('show a dummy', function(){
+
+	it('shows a dummy', function(){
 		const aDummy = {};
 
 		const sut = new Sut(aDummy);
 		
 		expect(sut instanceof Sut).toBeTruthy();
+	});
+
+	it('shows a stub', function(){
+		const clientValue = 24;
+		const aStub = { getValue: function(){return clientValue;} };
+		const sut = new Sut(aStub);
+
+		const result = sut.getValue();
+
 	});
 });
