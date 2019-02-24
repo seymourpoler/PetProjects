92392fa8351669cb5637c3272c79be76f30ae4d1
seymourpoler <seymourpoler@gmail.com>
2019-02-24 16:50:09 +0100

    red Jasmine Should throw_arguments_exception_when_test_suite_name_is_string_empty

--- JasmineDotNet/JasmineDotNet/JasmineDotNet/JasmineDotNet.Unit.Test/JasmineShould.cs
+++ JasmineDotNet/JasmineDotNet/JasmineDotNet/JasmineDotNet.Unit.Test/JasmineShould.cs
@@ -11,7 +11,17 @@ namespace JasmineDotNet.Unit.Test
         {
             Action testSuite = () => { var a = 2; };
 
-            Action action = () => new Jasmine().Describe(null,testSuite);
+            Action action = () => new Jasmine().Describe(null, testSuite);
+
+            action.ShouldThrow<ArgumentNullException>();
+        }
+        
+        [Fact]
+        public void throw_arguments_exception_when_test_suite_name_is_string_empty()
+        {
+            Action testSuite = () => { var a = 2; };
+
+            Action action = () => new Jasmine().Describe(String.Empty, testSuite);
 
             action.ShouldThrow<ArgumentNullException>();
         }
