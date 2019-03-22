47a7df12d0ff9bb8e387e0a0c107380e8a972f25
Seymour Poler <seymourpoler@gmail.com>
2019-03-22 22:05:05 +0100

    adding class Context

---
+++ JasmineDotNet/JasmineDotNet/JasmineDotNet/JasmineDotNet/Context.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JasmineDotNet
+{
+    public class Context
+    {
+        public string Name { get; }
+        private List<Context> contexts;
+        public IReadOnlyCollection<Context> Contexts { get { return contexts.AsReadOnly(); } }
+        private Action action;
+
+        public Context(string name, Action action)
+        {
+            Name = name;
+            this.action = action;
+            contexts = new List<Context>();
+        }
+
+        public void AddContext(Context context)
+        {
+            contexts.Add(context);
+        }
+    }
+}
