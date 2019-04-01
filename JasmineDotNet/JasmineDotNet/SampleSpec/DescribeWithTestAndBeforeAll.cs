7808034c74076e950790f247486b53978d84edd3
Seymour Poler <seymourpoler@gmail.com>
2019-04-01 15:09:18 +0200

    implementing beforeAll

--- JasmineDotNet/JasmineDotNet/SampleSpec/DescribeWithTestAndBeforeAll.cs
+++ JasmineDotNet/JasmineDotNet/SampleSpec/DescribeWithTestAndBeforeAll.cs
@@ -1,7 +1,34 @@
+using JasmineDotNet;
+
 namespace SampleSpec
 {
-    public class DescribeWithTestAndBeforeAll
+    public class DescribeWithTestAndBeforeAll : Jasmine
     {
-        
+        public void a_describe_with_a_success_test_and_forEach_method()
+        {
+            describe("the describe of a success test with for each method", () =>
+            {
+                var beforeAllWasThrough = false;
+                beforeAll(() => { beforeAllWasThrough = true;});
+
+                var value = 0;
+                beforeEach(() => { value++;});
+                it("the test with for each for each method", () => { expect(value).ToBe(1); });
+                it("another test with for each for each method", () => { expect(value).ToBe(2); });
+                it("before all has to be throguthn", () => {expect(beforeAllWasThrough).ToBe(true);});
+            });
+            
+            describe("the describe of a fail test with for each method", () =>
+            {
+                var beforeAllWasThrough = false;
+                beforeAll(() => { beforeAllWasThrough = true;});
+
+                var value = 0;
+                beforeEach(() => { value++;});
+                it("the test with for each for each method", () => { expect(true).ToBe(false); });
+                it("another test with for each for each method", () => { expect(value).ToBe(2); });
+                it("before all has to be throguthn", () => {expect(beforeAllWasThrough).ToBe(true);});
+            });
+        }
     }
 }
