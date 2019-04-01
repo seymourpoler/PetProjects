b6c10c1b9c66b56ec7f52371d61627f3340a5454
Seymour Poler <seymourpoler@gmail.com>
2019-04-01 20:15:37 +0200

    green throw_exception_when_is_true

--- JasmineDotNet/JasmineDotNet/JasmineDotNet/JasmineDotNet.Unit.Test/Expect_should.cs
+++ JasmineDotNet/JasmineDotNet/JasmineDotNet/JasmineDotNet.Unit.Test/Expect_should.cs
@@ -48,6 +48,17 @@ namespace JasmineDotNet.Unit.Test
         }
         
         [Fact]
+        public void throw_exception_when_is_true()
+        {
+            Action action = () =>
+            {
+                new Expected<bool>(true).Not.ToBeTrue();
+            };
+
+            action.ShouldThrow<ExpectException>();
+        }
+        
+        [Fact]
         public void do_nothing_when_is_not_true()
         {
             new Expected<bool>(false).Not.ToBeTrue();
