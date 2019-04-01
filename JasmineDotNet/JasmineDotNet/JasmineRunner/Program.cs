d6872935afec5094a6a761d9bc60b6809f730bfc
Seymour Poler <seymourpoler@gmail.com>
2019-04-01 18:14:32 +0200

    write all tests in the same suite

--- JasmineDotNet/JasmineDotNet/JasmineRunner/Program.cs
+++ JasmineDotNet/JasmineDotNet/JasmineRunner/Program.cs
@@ -6,18 +6,19 @@ namespace JasmineRunner
     {
         public static void Main(string[] args)
         {
-            //var path = "SampleSpec.dll";
             var path = args[0];
             var typeFinder = new TypeFinder();
             var contextFinder = new ContextFinder(new ClassFinder(new MethodFinder()));
             var specWritter = new SpecWritter(new ConsoleWritter());
             
             var types = typeFinder.find(path);
+            var context = new Context(path);
             foreach (var type in types)
             {
-                var context = contextFinder.Find(type);    
-                specWritter.Write(context);
+                var currentContext = contextFinder.Find(type);
+                context.AddContext(currentContext);
             }
+            specWritter.Write(context);
         }
     }
 }
