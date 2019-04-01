ec9b844d5b475b2c8b6d581db93d0caec59df32d
Seymour Poler <seymourpoler@gmail.com>
2019-04-01 20:27:57 +0200

    green do_nothing_whe_is_not_false

--- JasmineDotNet/JasmineDotNet/JasmineDotNet/JasmineDotNet.Unit.Test/Expect_should.cs
+++ JasmineDotNet/JasmineDotNet/JasmineDotNet/JasmineDotNet.Unit.Test/Expect_should.cs
@@ -82,7 +82,7 @@ namespace JasmineDotNet.Unit.Test
         }
         
         [Fact]
-        public void throw_exception_when_is_true()
+        public void throw_exception_when_is_not_true()
         {
             Action action = () =>
             {
@@ -96,17 +96,6 @@ namespace JasmineDotNet.Unit.Test
         public void do_nothing_when_is_not_true()
         {
             new Expected<bool>(false).Not.ToBeTrue();
-        }
-        
-        [Fact]
-        public void throw_exception_when_is_not_true()
-        {
-            Action action = () =>
-            {
-                new Expected<bool>(true).Not.ToBeTrue();
-            };
-
-            action.ShouldThrow<ExpectException>();
         }
         
         [Fact]
@@ -138,7 +127,13 @@ namespace JasmineDotNet.Unit.Test
         }
 
         [Fact]
-        public void do_nothing_when_is_not_to_be_false()
+        public void do_nothing_whe_is_not_false()
+        {
+            new Expected<bool>(true).Not.ToBeFalse();
+        }
+        
+        [Fact]
+        public void do_nothing_when_is_not_false()
         {
             new Expected<bool>(false).Not.ToBeTrue();
         }
