d9acec1934e61f38aedd62e4745e0cf562711924
Seymour Poler <seymourpoler@gmail.com>
2019-04-06 23:41:20 +0200

    update interface

--- JasmineDotNet/src/JasmineDotNet/Expects/IExpect.cs
+++ JasmineDotNet/src/JasmineDotNet/Expects/IExpect.cs
@@ -29,5 +29,7 @@
         void ToBeFalse();
         void ToThrow<T>();
         void ToContain(string value);
+        void ToBeGreaterThan(int number);
+        void ToBeLessThan(int number);
     }
 }
