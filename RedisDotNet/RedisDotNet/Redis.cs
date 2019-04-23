ef0fa3006462888a2ef05d1ca4e5feb30970fb64
Luis Rodríguez <seymourpoler@hotmail.com>
2019-04-23 15:30:59 +0200

    refactor: rename

--- RedisDotNet/RedisDotNet/Redis.cs
+++ RedisDotNet/RedisDotNet/Redis.cs
@@ -30,8 +30,8 @@ namespace RedisDotNet
             using (var socket = SocketFactory.Create(host: _host, port: _port))
             {
                 var dataToBeSent = BuildDataToBeSent(
-                    args: key, 
-                    data: Encoding.UTF8.GetBytes(value));
+                    keys: key, 
+                    values: Encoding.UTF8.GetBytes(value));
                 
                 socket.Send(Encoding.UTF8.GetBytes(dataToBeSent));
                 socket.Send(Encoding.UTF8.GetBytes(value));
@@ -39,16 +39,16 @@ namespace RedisDotNet
             }
         }
 
-        static string BuildDataToBeSent(byte [] data,  params object [] args)
+        static string BuildDataToBeSent(byte [] values,  params object [] keys)
         {
-            string resp = "*" + (1 + args.Length + 1).ToString () + "\r\n";
+            string resp = "*" + (1 + keys.Length + 1).ToString () + "\r\n";
             resp += "$3\r\nSET\r\n";
-            foreach (object arg in args) {
+            foreach (object arg in keys) {
                 string argStr = arg.ToString ();
                 int argStrLength = Encoding.UTF8.GetByteCount(argStr);
                 resp += "$" + argStrLength + "\r\n" + argStr + "\r\n";
             }
-            resp +=	"$" + data.Length + "\r\n";
+            resp +=	"$" + values.Length + "\r\n";
             return resp;
         }
     }
