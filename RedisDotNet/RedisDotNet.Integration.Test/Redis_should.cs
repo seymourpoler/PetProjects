658ce84823796285cb8e64b57fcf71bd68746321
Seymour Poler <seymourpoler@gmail.com>
2019-04-24 16:11:37 +0200

    fail set key and value

---
+++ RedisDotNet/RedisDotNet.Integration.Test/Redis_should.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace RedisDotNet.Integration.Test
+{
+    public class Redis_should
+    {
+        IRedis redis;
+
+        public Redis_should()
+        {
+            redis = RedisFactory.Create();
+        }
+
+        [Fact]
+        public void set_value()
+        {
+            redis.Set("foo", "bar");
+        }
+    }
+}
