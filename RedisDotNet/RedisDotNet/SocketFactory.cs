3eff0f7beb8658d4358bc23f39b6b07a33aff134
Seymour Poler <seymourpoler@gmail.com>
2019-04-28 00:27:17 +0200

    adding Coonectedsocket

--- RedisDotNet/RedisDotNet/SocketFactory.cs
+++ RedisDotNet/RedisDotNet/SocketFactory.cs
@@ -4,13 +4,9 @@ namespace RedisDotNet
 {
     public static class SocketFactory
     {
-        public static Socket Create(string host, int port)
+        public static ConnectedSocket Create(string host, int port)
         {
-            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            socket.NoDelay = true;
-            socket.SendTimeout = -1;
-            socket.Connect(host: host, port: port);
-            
+            var socket = new ConnectedSocket(host: host, port: port);
             return socket;
         }
     }
