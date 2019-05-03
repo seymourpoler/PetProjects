455ede9a84b5b1c8d8b76333ba148ff40de98676
Luis Rodríguez <seymourpoler@hotmail.com>
2019-05-03 14:28:01 +0200

    remove command

--- RedisDotNet/RedisDotNet/Commands/Remove.cs
+++ RedisDotNet/RedisDotNet/Commands/Remove.cs
@@ -8,7 +8,7 @@ namespace RedisDotNet.Commands
         public Remove(string host, int port) 
             : base(host: host, port: port) { }
 
-        public int Execute(string key)
+        public void Execute(string key)
         {
             var dataToBeSent = new StringBuilder("*2\r\n");
             dataToBeSent.Append("$3\r\nDEL\r\n");
@@ -19,9 +19,19 @@ namespace RedisDotNet.Commands
             dataToBeSent.Append("\r\n");
             var bytes = Encoding.UTF8.GetBytes(dataToBeSent.ToString());
             _socket.Send(bytes);
-            
-            var c = _buffer.ReadByte ();
-            throw new NotImplementedException();
+
+            var currentReadByteResult = _buffer.ReadByte(); 
+            if (currentReadByteResult == fail)
+            {
+                throw new RedisException("not remove key");
+            }
+
+            const string success = "1";
+            var line = ReadLine();
+            if (line != success)
+            {
+                throw new RedisException(line.StartsWith ("ERR ") ? line.Substring (4) : line);
+            }
         }
     }
 }
