804cb438649213ff6410ef1e91f9ea6e1e523ee6
Luis Rodríguez <seymourpoler@hotmail.com>
2019-05-27 18:46:13 +0200

    rename

--- growing_object-oriented_software_guided_by_tests/test/TranslatorListenerShould.java
+++ growing_object-oriented_software_guided_by_tests/test/TranslatorListenerShould.java
@@ -5,7 +5,7 @@ import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-public class ListenerShould {
+public class TranslatorListenerShould {
 
     Translator translator;
     Message message;
