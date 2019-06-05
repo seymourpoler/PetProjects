109036a2056154a90c3e73a6921c080213b23a17
Luis Rodríguez <seymourpoler@hotmail.com>
2019-06-05 18:46:51 +0200

    fix error

--- growing_object-oriented_software_guided_by_tests/src/UI.java
+++ growing_object-oriented_software_guided_by_tests/src/UI.java
@@ -2,6 +2,6 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class UI {
     public void showStatus(String status){
-        throw  new NotImplementedException()M
+        throw  new NotImplementedException();
     }
 }
