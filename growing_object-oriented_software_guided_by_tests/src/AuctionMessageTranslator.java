762f72e0b8026952f481faa458868eacf03d2d49
Luis Rodríguez <seymourpoler@hotmail.com>
2019-06-10 19:05:55 +0200

    adding processMessage method

--- growing_object-oriented_software_guided_by_tests/src/AuctionMessageTranslator.java
+++ growing_object-oriented_software_guided_by_tests/src/AuctionMessageTranslator.java
@@ -1,2 +1,8 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 public class AuctionMessageTranslator {
+
+    public void processMessage(Chat aChat, Message aMessage){
+        throw new NotImplementedException();
+    }
 }
--- growing_object-oriented_software_guided_by_tests/test/AuctionMessageTranslatorTest.java
+++ growing_object-oriented_software_guided_by_tests/test/AuctionMessageTranslatorTest.java
@@ -4,6 +4,11 @@ public class AuctionMessageTranslatorTest {
     @Test
     public void notifiesAuctionClosedWhenCloseMessageReceived()
     {
+        Chat aChat = null;
+        AuctionMessageTranslator translator = new AuctionMessageTranslator();
+        Message aMessage  = new Message();
+        aMessage.setBody("SOLVersion: 1.1; Event: CLOSE;");
 
+        translator.processMessage(aChat, aMessage);
     }
 }
