d012fc724d7e64ba9343cd8064e934b0cfb2a338
seymourpoler <seymourpoler@gmail.com>
2019-06-10 20:41:25 +0200

    green notifiesBidDetailsWhenCurrentPriceMessageReceived

--- growing_object-oriented_software_guided_by_tests/src/AuctionEventListener.java
+++ growing_object-oriented_software_guided_by_tests/src/AuctionEventListener.java
@@ -1,3 +1,4 @@
 public interface AuctionEventListener {
      void auctionClosed();
+     void currentPrice(int price, int increment);
 }
--- growing_object-oriented_software_guided_by_tests/src/AuctionMessageTranslator.java
+++ growing_object-oriented_software_guided_by_tests/src/AuctionMessageTranslator.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class AuctionMessageTranslator {
 
     private AuctionEventListener listener;
@@ -7,6 +9,22 @@ public class AuctionMessageTranslator {
     }
 
     public void processMessage(Chat aChat, Message aMessage){
-        listener.auctionClosed();
+        HashMap<String, String> event = unpackEventFrom(aMessage);
+        String type = event.get("Event");
+        if ("CLOSE".equals(type)) {
+            listener.auctionClosed();
+        } else if ("PRICE".equals(type)) {
+            listener.currentPrice(Integer.parseInt(event.get("CurrentPrice")),
+                    Integer.parseInt(event.get("Increment")));
+        }
+    }
+
+    private HashMap<String, String> unpackEventFrom(Message message) {
+        HashMap<String, String> event = new HashMap<String, String>();
+        for (String element : message.getBody().split(";")) {
+            String[] pair = element.split(":");
+            event.put(pair[0].trim(), pair[1].trim());
+        }
+        return event;
     }
 }
