873af8fa97ec12ec57dd37737736dac8fdd8e990
Luis Rodríguez <seymourpoler@hotmail.com>
2019-06-10 18:58:26 +0200

    adding an empty test: notifiesAuctionClosedWhenCloseMessageReceived()

--- growing_object-oriented_software_guided_by_tests/test/AuctionMessageTranslatorTest.java
+++ growing_object-oriented_software_guided_by_tests/test/AuctionMessageTranslatorTest.java
@@ -1,2 +1,9 @@
+import org.junit.jupiter.api.Test;
+
 public class AuctionMessageTranslatorTest {
+    @Test
+    public void notifiesAuctionClosedWhenCloseMessageReceived()
+    {
+        
+    }
 }
