9cb1fa0dbe910f98b1c2da7865113c34cc59e0b1
Seymour Poler <seymourpoler@gmail.com>
2019-07-10 19:52:39 +0200

    directory watche

--- groovy_koans/run.sh
+++ groovy_koans/run.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-while true; do 
-    printf "\033c" #clear screen
-    groovy AllKoans.groovy
-    sleep 5;
-done
+# while true; do 
+#    printf "\033c" #clear screen
+#    groovy AllKoans.groovy
+#    sleep 5;
+#done
+
+
+
+chsum1=""
+
+    while [[ true ]]
+    do
+        chsum2=`find koans/ -type f -exec md5sum {} \;`
+        if [[ $chsum1 != $chsum2 ]] ; then
+        	printf "\033c" #clear screen          
+            groovy AllKoans.groovy
+            chsum1=$chsum2
+        fi
+        sleep 2
+    done
