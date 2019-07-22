8cd5debca5e973e6158b1ab5378ad3b568e1acd9
Seymour Poler <seymourpoler@gmail.com>
2019-07-22 20:14:21 +0200

    adding properties

--- TekDays/grails-app/domain/tekdays/TekEvent.groovy
+++ TekDays/grails-app/domain/tekdays/TekEvent.groovy
@@ -2,6 +2,18 @@ package tekdays
 
 class TekEvent {
 
+    String city
+    String name
+    String organizer
+    String venue
+    Date startDate
+    Date endDate
+    String description
+
+    String toString(){
+        "$name, city"
+    }
+
     static constraints = {
     }
 }
