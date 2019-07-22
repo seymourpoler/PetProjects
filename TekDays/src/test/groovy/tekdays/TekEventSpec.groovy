e2c9c22cb4765af24e0c03cecf472da5f9b8c4dd
Seymour Poler <seymourpoler@gmail.com>
2019-07-22 20:25:24 +0200

    green test toString

--- TekDays/src/test/groovy/tekdays/TekEventSpec.groovy
+++ TekDays/src/test/groovy/tekdays/TekEventSpec.groovy
@@ -9,6 +9,20 @@ class TekEventSpec extends Specification implements DomainUnitTest<TekEvent> {
     }
 
     def cleanup() {
+
+    }
+
+    void "test toString"(){
+        def event = new TekEvent(
+                name: "Groovy One",
+                city: "San Francisco, co",
+                organizer: "John Doe",
+                venue: "Moscone Center",
+                startDate: new Date("06/02/2009"),
+                endDate:  new Date("06/05/2009"),
+                description: "this conference cover ...")
+
+        assertEquals "Groovy One, San Francisco, co", event.toString()
     }
 
     void "test something"() {
