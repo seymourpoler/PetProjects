47b4e0a273093c67fa49eded140d77a67eb07f6a
Seymour Poler <seymourpoler@gmail.com>
2019-07-26 08:10:21 +0200

    add to bootstrap

--- TekDays/grails-app/conf/BootStrap.groovy
+++ TekDays/grails-app/conf/BootStrap.groovy
@@ -4,6 +4,29 @@ class BootStrap {
 
     def init = { servletContext ->
     	
+		new TekUser(
+			fullName: 'John Doe',
+			userName: 'jdoe',
+			password: 't0ps3cr3t',
+			email: 'jdoe@johnsgroovyshop.com',
+			website: 'blog.johnsgroovyshop.com',
+			bio: '''John has been programming for over 40 years. He has
+				worked with every programming language known to man
+				and has settled on Groovy. In his spare time, John
+				dabbles in astro physics and plays
+				shuffleboard.''').save()
+
+		new TekUser(
+			fullName: 'John Deere',
+			userName: 'tractorman',
+			password: 't0ps3cr3t',
+			email: 'john.deere@porkproducers.org',
+			website: 'www.perl.porkproducers.org',
+			bio: '''John is a top notch Perl programmer and a pretty
+				good hand around the farm. If he can't program it he
+				can plow it!''').save()
+
+
     	def event1 = new TekEvent(
     		name: 'Gateway Code Camp',
 			city: 'Saint Louis, MO',
@@ -17,12 +40,7 @@ class BootStrap {
 				and tech! Stay sharp! Stay at the top of your
 				game! But, don't stay home! Come an join us
 				this fall for the first annual Gateway Code
-				Camp.''')
-    	if(!event1.save()){
-			event1.errors.allErrors.each{error ->
-				println "An error occured with event1: ${error}"
-			}
-		}
+				Camp.''').save()
 
 		
 		def event2 = new TekEvent(
@@ -39,36 +57,7 @@ class BootStrap {
 				chops in our programming challenge.
 				Come and join us in historic (and aromatic),
 				Austin, Minnesota. You'll know when you're
-				there!''')
-		if(!event2.save()){
-			event2.errors.allErrors.each{error ->
-				println "An error occured with event2: ${error}"
-			}
-		}
-
-		new TekUser(
-			fullName: 'John Doe',
-			userName: 'jdoe',
-			password: 't0ps3cr3t',
-			email: 'jdoe@johnsgroovyshop.com',
-			website: 'blog.johnsgroovyshop.com',
-			bio: '''John has been programming for over 40 years. He has
-				worked with every programming language known to man
-				and has settled on Groovy. In his spare time, John
-				dabbles in astro physics and plays
-				shuffleboard.''')
-		.save()
-
-		new TekUser(
-			fullName: 'John Deere',
-			userName: 'tractorman',
-			password: 't0ps3cr3t',
-			email: 'john.deere@porkproducers.org',
-			website: 'www.perl.porkproducers.org',
-			bio: '''John is a top notch Perl programmer and a pretty
-				good hand around the farm. If he can't program it he
-				can plow it!''')
-		.save()
+				there!''').save()
 	}
     	
     def destroy = {
