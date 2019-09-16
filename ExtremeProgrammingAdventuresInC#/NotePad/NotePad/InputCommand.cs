dea33f3def815ad54c2ab3efddd3e9e023dace78
Luis Rodríguez <seymourpoler@hotmail.com>
2019-09-16 18:28:58 +0200

    green EmptyCommand

---
+++ ExtremeProgrammingAdventuresInC#/NotePad/NotePad/InputCommand.cs
@@ -0,0 +1,7 @@
+namespace NotePad
+{
+    public class InputCommand
+    {
+        
+    }
+}
---
+++ ExtremeProgrammingAdventuresInC#/NotePad/NotePad/InputCommandTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace NotePad
+{
+    [TestFixture]
+    public class InputCommandTest
+    {
+        InputCommand command;
+        [Test]
+        public void EmptyCommand()
+        {
+            command = new InputCommand();
+            
+            Assert.IsTrue(1 == 1);
+        }
+        
+    }
+}
