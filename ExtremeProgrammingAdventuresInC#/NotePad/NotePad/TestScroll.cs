9de7958c637152b65f195a43c9b0bcf976aaf516
Seymour Poler <seymourpoler@gmail.com>
2019-10-03 18:59:38 +0200

    green TestScroll HookUp

---
+++ ExtremeProgrammingAdventuresInC#/NotePad/NotePad/TestScroll.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace NotePad
+{
+    [TestFixture]
+    public class TestScroll
+    {
+        [Test]
+        public void HookUp()
+        {
+            Assert.IsTrue(true, "hooked up");
+        }
+    }
+}
