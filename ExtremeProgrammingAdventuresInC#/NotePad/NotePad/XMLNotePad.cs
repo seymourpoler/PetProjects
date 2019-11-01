bbf77e1c5e968358ae88dececc66828dfaf324df
Seymour Poler <seymourpoler@gmail.com>
2019-11-01 16:48:24 +0100

    adding TesteableTextBox property

--- ExtremeProgrammingAdventuresInC#/NotePad/NotePad/XMLNotePad.cs
+++ ExtremeProgrammingAdventuresInC#/NotePad/NotePad/XMLNotePad.cs
@@ -1,20 +1,34 @@
 using System.Windows.Forms;
 using System;
+using System.Drawing;
 
 namespace NotePad
 {
     public class XMLNotePad : NotePadCloneNoMenu
     {
+        public TesteableTextBox textbox;
         private TextModel model;
         
         public XMLNotePad()
         {
             Text = "XML Notepad";
+            textbox = new TesteableTextBox();
+            textbox.Parent = this;
+            textbox.Dock = DockStyle.Fill;
+            textbox.BorderStyle = BorderStyle.None;
+            textbox.Multiline = true;
+            textbox.ScrollBars = ScrollBars.Both;
+            textbox.AcceptsTab = true;
             textbox.KeyDown += XMLKeyDownHandler;
             textbox.KeyPress += XMLKeyPressHandler;
             model = new TextModel();
+            this.textbox.Visible = true;
+            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+            this.ClientSize = new System.Drawing.Size(292, 66);
+            this.Controls.AddRange(new System.Windows.Forms.Control[] {this.textbox});
+            this.Name = "XMLNotePad";
         }
-
+        
         void XMLKeyPressHandler(object sender, KeyPressEventArgs kea)
         {
             if (kea.KeyChar == (int) Keys.Enter)
@@ -31,10 +45,14 @@ namespace NotePad
                 model.Enter();
                 kea.Handled = true;
             }
-
+            if (kea.KeyCode == Keys.X)
+            {
+                kea.Handled = true;
+            }
             if (kea.KeyCode == Keys.S && kea.Alt)
             {
                 model.AltS();
+                kea.Handled = true;
             }
             PutText(textbox, model.Lines, model.SelectionStart);
         }
