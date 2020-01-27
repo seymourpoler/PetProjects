fc1027ddcd9663fe7320630c7101173866dd14e2
Luis G. Rodriguez <lgabriel@idealista.com>
2020-01-27 18:48:00 +0100

    green return_an_empty_list_when_code_is_string_empty

--- jascal/src/test/java/jascal/LexerShould.java
+++ jascal/src/test/java/jascal/LexerShould.java
@@ -12,4 +12,11 @@ public class LexerShould {
 
         List<Token> tokens = lexer.scan(null);
     }
+
+    @Test
+    public void return_an_empty_list_when_code_is_string_empty(){
+        Lexer lexer = new Lexer();
+
+        List<Token> tokens = lexer.scan("");
+    }
 }
