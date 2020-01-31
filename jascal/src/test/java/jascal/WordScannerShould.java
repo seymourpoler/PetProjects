e3b2e7ff25d0c6428151a12b0a36fbfc7757b0d7
Luis G. Rodriguez <lgabriel@idealista.com>
2020-01-31 08:57:34 +0100

    refactor: red return_program_reserved_word

--- jascal/src/test/java/jascal/WordScannerShould.java
+++ jascal/src/test/java/jascal/WordScannerShould.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -19,5 +20,24 @@ public class WordScannerShould {
         List<Word> words = wordScanner.scan();
 
         assertTrue(words.isEmpty());
+    }
+
+    @Test
+    public void return_program_reserved_word(){
+        TextReader textReader = mock(TextReader.class);
+        when(textReader.isEmpty()).thenReturn(false);
+        when(textReader.getCurretCharacter())
+                .thenReturn("P")
+                .thenReturn("R")
+                .thenReturn("O")
+                .thenReturn("G")
+                .thenReturn("R")
+                .thenReturn("A")
+                .thenReturn("M");
+        WordScanner wordScanner = new WordScanner(textReader);
+
+        List<Word> words = wordScanner.scan();
+
+        assertEquals(words.get(0), PorgramReservedWord.class);
     }
 }
