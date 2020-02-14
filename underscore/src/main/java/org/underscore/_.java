7e2aa30c7cf1295bc2de930e51373678d90ee15d
seymourpoler <seymourpoler@gmail.com>
2020-02-14 16:25:36 +0100

    starting with underscore in java

---
+++ underscore/src/main/java/org/underscore/_.java
@@ -0,0 +1,84 @@
+package org.underscore;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class _ {
+    public static <T> void each(List<T> elements, Consumer<T> consumer){
+        for(T element: elements){
+            consumer.accept(element);
+        }
+    }
+
+    public static <T> List<T> where(List<T> elements, Function<T, Boolean> condition){
+        List<T> result = new ArrayList<T>();
+        for (T element: elements){
+            if(condition.apply(element)){
+                result.add(element);
+            }
+        }
+        return result;
+    }
+
+    public static <T> List<T> where(List<T> elements, Predicate<T> condition){
+        List<T> result = new ArrayList<T>();
+        for (T element: elements){
+            if(condition.test(element)){
+                result.add(element);
+            }
+        }
+        return result;
+    }
+
+    public static <T> Optional<T> find(List<T> elements, Function<T, Boolean> condition){
+        for (T element: elements){
+            if (condition.apply(element)) {
+                return Optional.of(element);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public static <T, R> List<R> map(List<T> elements, Function<T, R> mapper){
+        List<R> result = new ArrayList<R>();
+        for (T element: elements) {
+            result.add(mapper.apply(element));
+        }
+        return result;
+    }
+
+    public static <T> Optional<T> first(List<T> elements){
+        if(isNull(elements) || isEmpty(elements)){
+            return Optional.empty();
+        }
+        return Optional.of(elements.get(0));
+    }
+
+    public static <T> Optional<T> last(List<T> elements){
+        if(isNull(elements) || isEmpty(elements)){
+            return Optional.empty();
+        }
+        return Optional.of(elements.get(elements.size() -1));
+    }
+
+    public static <T> Boolean isEmpty(List<T> elements){
+        final Integer hasNoElements = 0;
+        return elements.size() == hasNoElements;
+    }
+
+    public static <T> Boolean isNotEmpty(List<T> elements){
+        return !isEmpty(elements);
+    }
+
+    public static <T> Boolean isNull(List<T> elements){
+        return elements == null;
+    }
+
+    public static <T> Boolean isNotNull(List<T> elements){
+        return !isNull(elements);
+    }
+}
---
+++ underscore/src/test/java/org/underscore/_Should.java
@@ -0,0 +1,160 @@
+package org.underscore;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class _Should {
+    @Test
+    public void each(){
+        Boolean[] isExecuted = new Boolean[1];
+        List<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+
+        _.each(numbers, x -> isExecuted[0] = true);
+
+        Assert.assertTrue(isExecuted[0]);
+    }
+
+    @Test
+    public void where_with_lambda_function(){
+        List<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        Function<Integer, Boolean> condition = x -> (x % 2) == 0;
+
+        List<Integer> result = _.where(numbers, condition);
+
+        Assert.assertEquals(2, result.size());
+    }
+
+    @Test
+    public void where_with_lambda_predicate(){
+        List<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        Predicate<Integer> condition = x -> (x % 2) == 0;
+
+        List<Integer> result = _.where(numbers, condition);
+
+        Assert.assertEquals(2, result.size());
+    }
+
+    @Test
+    public void find_an_element(){
+        List<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        final Integer filter = 2;
+        Function<Integer, Boolean> condition = x -> x == filter;
+
+        Optional<Integer> result = _.find(numbers, condition);
+
+        Assert.assertEquals(Optional.of(filter), result);
+    }
+
+    @Test
+    public void not_find_an_element(){
+        List<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        Function<Integer, Boolean> condition = x -> x == 20;
+
+        Optional<Integer> result = _.find(numbers, condition);
+
+        Assert.assertFalse(result.isPresent());
+    }
+
+    @Test
+    public void return_true_when_is_empty(){
+        List<Integer> numbers = new ArrayList<>();
+
+        Assert.assertTrue(_.isEmpty(numbers));
+    }
+
+    @Test
+    public void return_true_when_is_not_empty(){
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(2);
+
+        Assert.assertTrue(_.isNotEmpty(numbers));
+    }
+
+    @Test
+    public void return_first(){
+        List<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+
+        Optional<Integer> result = _.first(numbers);
+
+        Assert.assertEquals(Optional.of(1), result);
+    }
+
+    @Test
+    public void no_return_first(){
+        List<Integer> numbers = new ArrayList<Integer>();
+
+        Optional<Integer> result = _.first(numbers);
+
+        Assert.assertEquals(Optional.empty(), result);
+    }
+
+    @Test
+    public void return_last_element(){
+        List<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+
+        Optional<Integer> result = _.last(numbers);
+
+        Assert.assertEquals(Optional.of(2), result);
+    }
+
+    @Test
+    public void return_no_last_element(){
+        List<Integer> numbers = new ArrayList<Integer>();
+        Optional<Integer> result = _.last(numbers);
+
+        Assert.assertEquals(Optional.empty(), result);
+    }
+
+    @Test
+    public void return_true_when_is_null(){
+        Assert.assertTrue(_.isNull(null));
+    }
+
+    @Test
+    public void return_true_when_is_not_null(){
+        List<Integer> numbers = new ArrayList<Integer>();
+
+        Assert.assertTrue(_.isNotNull(numbers));
+    }
+
+    @Test
+    public void map(){
+        List<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        Function<Integer, Integer> mapper = x ->  x + 1;
+        List<Integer> result = _.map(numbers, mapper);
+
+        Assert.assertTrue( result.get(0).equals(2));
+    }
+}
