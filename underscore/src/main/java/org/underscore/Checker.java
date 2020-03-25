08b66e58396d310d9fa137e3923babe629b4d04f
Luis G. Rodriguez <lgabriel@idealista.com>
2020-03-25 18:51:34 +0100

    red throw_exception_when_is_null

---
+++ underscore/src/main/java/org/underscore/Checker.java
@@ -0,0 +1,10 @@
+package org.underscore;
+
+public class Checker {
+    public static <T, U> void ifNull(Class<T> clazz, U element)
+            throws Throwable {
+//        if(element == null){
+//            throw (Throwable) clazz.newInstance();
+//        }
+    }
+}
