f4bccaf72afbc631084f5d582bebe2e9a9d6a471
Seymour Poler <seymourpoler@gmail.com>
2020-04-24 17:08:03 +0200

    use StringUtil class

--- jDataBaseMigrator/src/main/java/net/seymourpoler/Check.java
+++ jDataBaseMigrator/src/main/java/net/seymourpoler/Check.java
@@ -2,7 +2,7 @@ package net.seymourpoler;
 
 public class Check {
     public static void isNullOrWhiteSpace(String value) {
-        if(value == null || value.equals("") || value.trim().equals("")){
+        if(StringUtil.isNullOrWhiteSpace(value)){
             throw new IllegalArgumentException();
         }
     }
