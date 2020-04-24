f0c1bf4b58de1dc0db475fbf0a9b5b8f27ff7edf
Seymour Poler <seymourpoler@gmail.com>
2020-04-24 16:24:38 +0200

    refactor rename

--- jDataBaseMigrator/src/test/java/RenameTableShould.java
+++ jDataBaseMigrator/src/test/java/RenameTableShould.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 public class RenameTableShould {
     @Test(expected = IllegalArgumentException.class)
     public void
-    throw_exeception_when_old_name_is_null(){
+    throw_exception_when_old_name_is_null(){
         var renameTable = new RenameTable(null, "new_table");
 
         renameTable.toSql();
