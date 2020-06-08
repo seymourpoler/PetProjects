c25dbc8a4ca7d0dc219154022d15e170fd0bec34
Luis G. Rodriguez <lgabriel@idealista.com>
2020-06-08 17:24:15 +0200

    not testedimplementing post method

--- TuduManayer/java/TuduManager/web.react/src/http.js
+++ TuduManayer/java/TuduManager/web.react/src/http.js
@@ -2,10 +2,26 @@ export function Http(){
     let self = this;
 	
 	self.get = async (url) => {
-   		const response = await fetch('GET', url);
+		const response = await fetch(url);
+		//TODO: duplicated code extract method
 		return {
 			statusCode: response.statusCode,
-			data: response.json()};
+			data: response.json()
+		};
+	};
+
+	self.post = (url) => {
+		const response = await fetch(url, {
+			method: 'POST',
+  			headers: {
+				'Content-Type': 'application/json;charset=utf-8'
+			}
+		}
+		//TODO: duplicated code extract method
+		return {
+			statusCode: response.statusCode,
+			data: response.json()           		
+		};                                      	
 	}
 
 }
