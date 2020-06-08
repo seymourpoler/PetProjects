9877e335eccf4620c27b7fa8bf8fef474c7838ee
Luis G. Rodriguez <lgabriel@idealista.com>
2020-06-08 17:10:54 +0200

    implementing get method with fetch

--- TuduManayer/java/TuduManager/web.react/src/http.js
+++ TuduManayer/java/TuduManager/web.react/src/http.js
@@ -1,2 +1,11 @@
 export function Http(){
+    let self = this;
+	
+	self.get = async (url) => {
+   		const response = await fetch('GET', url);
+		return {
+			statusCode: response.statusCode,
+			data: response.json()};
+	}
+
 }
