80074bfeebc8d37f54e4b7f52c1a5a3d439fb60a
Luis G. Rodriguez <lgabriel@idealista.com>
2020-06-16 15:49:31 +0200

    remove not needed code

--- TuduManayer/java/TuduManager/web.api.spring.boot/src/main/java/net/seymourpoler/tudumanager/web/api/spring/boot/Application.java
+++ TuduManayer/java/TuduManager/web.api.spring.boot/src/main/java/net/seymourpoler/tudumanager/web/api/spring/boot/Application.java
@@ -1,13 +1,9 @@
 package net.seymourpoler.tudumanager.web.api.spring.boot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-//@Configuration
-//@EnableAutoConfiguration
 @ComponentScan(basePackages="net.seymourpoler.tudumanager")
 @SpringBootApplication
 public class Application {
