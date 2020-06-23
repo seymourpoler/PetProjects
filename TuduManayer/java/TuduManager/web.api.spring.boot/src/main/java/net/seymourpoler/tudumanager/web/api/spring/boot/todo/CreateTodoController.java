1387de6b26770506daf0acd809d7e707b0f66076
Luis G. Rodriguez <lgabriel@idealista.com>
2020-06-23 15:15:45 +0200

    adding cotroller

---
+++ TuduManayer/java/TuduManager/web.api.spring.boot/src/main/java/net/seymourpoler/tudumanager/web/api/spring/boot/todo/CreateTodoController.java
@@ -0,0 +1,7 @@
+package net.seymourpoler.tudumanager.web.api.spring.boot.todo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CreateTodoController {
+}
