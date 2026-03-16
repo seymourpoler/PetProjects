35d7d9acd7a345c856a5191f3fbd00d922eb456d
Luis Rodríguez <luis.rodriguez@leanmind.es>
2026-03-16 19:49:28 +0100

    refactor: initialize dependencies in TokenizerShould constructor

--- TSharp/test/TSharp.Test/Lexer/TokenizerShould.cs
+++ TSharp/test/TSharp.Test/Lexer/TokenizerShould.cs
@@ -5,12 +5,19 @@ namespace TSharp.Test.Lexer;
 
 public class TokenizerShould
 {
+    private readonly FileReader fileReader;
+    private readonly Tokenizer tokenizer;
+
+    public TokenizerShould()
+    {
+        fileReader = A.Fake<FileReader>();
+        tokenizer = new Tokenizer(fileReader);
+    }
+    
     [Fact]
     public void ReturnEmptyListForEmptyInput()
     {
-        var fileReader = A.Fake<FileReader>();
         A.CallTo(() => fileReader.IsAtTheEnd()).Returns(true);
-        var tokenizer = new Tokenizer(fileReader);
         
         var tokens = tokenizer.Tokenize();
         
