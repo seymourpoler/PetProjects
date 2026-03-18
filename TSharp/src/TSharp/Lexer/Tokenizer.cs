11837a3264ca4ee7e6957ce8d6d6e6ab436a7995
Luis Rodríguez <luis.rodriguez@leanmind.es>
2026-03-18 17:29:32 +0100

    feat: enhance Tokenizer to support number and symbol tokenization

--- TSharp/src/TSharp/Lexer/Tokenizer.cs
+++ TSharp/src/TSharp/Lexer/Tokenizer.cs
@@ -2,16 +2,59 @@
 
 public class Tokenizer(FileReader fileReader)
 {
-        public IEnumerable<Token> Tokenize()
+        public List<Token> Tokenize()
         {
             var tokens = new List<Token>();
             
-            while (!fileReader.IsAtTheEnd())
+        while (!fileReader.IsAtTheEnd())
+        {
+            var ch = fileReader.Next();
+            if (char.IsWhiteSpace(ch[0]))
+                continue;
+            // Keyword or identifier
+            if (char.IsLetter(ch[0]))
             {
-                
+                var start = ch[0];
+                var lexeme = start.ToString();
+                while (!fileReader.IsAtTheEnd() && char.IsLetterOrDigit(fileReader.Current()[0]))
+                {
+                    lexeme += fileReader.Next();
+                }
+                // Check for keyword const
+                if (lexeme == "const")
+                    tokens.Add(new Token(TokenType.Constant, lexeme, fileReader.LineNumber()));
+                else
+                    tokens.Add(new Token(TokenType.Identifier, lexeme, fileReader.LineNumber()));
+                continue;
             }
-            tokens.Add(new Token(TokenType.EndOfFile, string.Empty, fileReader.LineNumber()));
-            
-            return tokens;
+            // Number
+            if (char.IsDigit(ch[0]))
+            {
+                var lexeme = ch[0].ToString();
+                while (!fileReader.IsAtTheEnd() && char.IsDigit(fileReader.Current()[0]))
+                {
+                    lexeme += fileReader.Next();
+                }
+                tokens.Add(new Token(TokenType.Number, lexeme, fileReader.LineNumber()));
+                continue;
+            }
+            // Symbols
+            switch (ch)
+            {
+                case "=":
+                    tokens.Add(new Token(TokenType.Equal, ch, fileReader.LineNumber()));
+                    break;
+                case ";":
+                    tokens.Add(new Token(TokenType.Semicolon, ch, fileReader.LineNumber()));
+                    break;
+                default:
+                    // Ignore unknown for now
+                    break;
+            }
         }
-}
+        tokens.Add(new Token(TokenType.EndOfFile, string.Empty, fileReader.LineNumber()));
+
+                    
+                    return tokens;
+                }
+        }
--- TSharp/test/TSharp.Test/Lexer/TokenizerShould.cs
+++ TSharp/test/TSharp.Test/Lexer/TokenizerShould.cs
@@ -9,12 +9,40 @@ public class TokenizerShould
     private readonly FileReader fileReader;
     private readonly Tokenizer tokenizer;
 
+
     public TokenizerShould()
     {
         fileReader = A.Fake<FileReader>();
         tokenizer = new Tokenizer(fileReader);
     }
-    
+
+    [Fact]
+    public void Tokenize_ConstDeclaration_ReturnsCorrectTokens()
+    {
+        const int lineNumber = 1;
+        var chars = new Queue<char>("const a = 4;");
+        A.CallTo(() => fileReader.IsAtTheEnd()).ReturnsLazily(() => chars.Count == 0);
+        A.CallTo(() => fileReader.Next()).ReturnsLazily(() => chars.Count > 0 ? chars.Dequeue().ToString() : "");
+        A.CallTo(() => fileReader.Current()).ReturnsLazily(() => chars.Count > 0 ? chars.Peek().ToString() : "");
+        A.CallTo(() => fileReader.LineNumber()).Returns(lineNumber);
+        
+        var tokens = tokenizer.Tokenize().ToList();
+        
+        tokens.Count.ShouldBe(6);
+        tokens[0].Type.ShouldBe(TokenType.Constant);
+        tokens[0].Lexeme.ShouldBe("const");
+        tokens[1].Type.ShouldBe(TokenType.Identifier);
+        tokens[1].Lexeme.ShouldBe("a");
+        tokens[2].Type.ShouldBe(TokenType.Equal);
+        tokens[2].Lexeme.ShouldBe("=");
+        tokens[3].Type.ShouldBe(TokenType.Number);
+        tokens[3].Lexeme.ShouldBe("4");
+        tokens[4].Type.ShouldBe(TokenType.Semicolon);
+        tokens[4].Lexeme.ShouldBe(";");
+        tokens[5].Type.ShouldBe(TokenType.EndOfFile);
+    }
+
+
     [Fact]
     public void ReturnEmptyListForEmptyInput()
     {
